eae7c69c43ec79e369b371b6a06640dccdb691af
Thiago Granville <thiagogranville@gmail.com>
2021-02-12 17:30:24 -0300

    Requisito 9: Renderiza subtitulos em tags H5

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,8 +6,9 @@ class MovieCard extends React.Component {
     const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <div className="movie-card">
-        <img src={imagePath} alt={title} />
-        <h4>{title}</h4>
+        <img src={ imagePath } alt={ title } />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
       </div>
     );
   }
