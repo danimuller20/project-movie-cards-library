dcc4840a6a083b3d5f7ceb9aaa6fe1a82566c419
LucioOSilva <lucioosilva00@gmail.com>
2021-02-12 15:08:38 -0300

    React Component- Rating -adjusts eslint2

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,15 +1,19 @@
 // implement Rating component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
     const { rate } = this.props.rating;
     return (
-      <p>Rating
+      <p>
+        Rating
         <span className='rating'>{rate}</span>
       </p>
     );
   }
 }
+
+Rating.proptypes = { rate: PropTypes.number };
 
 export default Rating;
