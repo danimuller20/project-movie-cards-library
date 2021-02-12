0001f8e36a273edde2a9789f1b506ad3530ee676
ggaldino95 <ggaldino95@gmail.com>
2021-02-12 16:01:53 -0300

    [Task 3, 4 and 5] MovieList Component implemented and rendering MovieCards from it with individual keys

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    return (
+      <div>
+        <MovieCard key="Movie Title 1" />
+        <MovieCard key="Movie Title 2" />
+        <MovieCard key="Movie Title 3" />
+      </div>
+    );
+  }
+}
+
+export default MovieList;
