ad457700bd926dde02930c0ee9c22d999c73b94a
edison-ps <edison@osite.com.br>
2021-02-12 20:06:18 -0300

    Sextounpm run lint

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './App.css';
+import MovieList from './components/MovieList';
+import movies from './data';
 
 function App() {
   return (
     <div className="App">
-      {
-        <Header />
-      }
+      <Header />
+      <MovieList movies={ movies } />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    return (
+      <div>
+        { this.props.movies.map((movieItem) => {
+          <MovieCard movie={ movieItem } key={ movieItem.title } />
+          })
+        }   
+      </div>
+    )
+  }
+}
+  export default MovieList;
