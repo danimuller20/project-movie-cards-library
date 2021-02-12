30b35e0b282802f395f167d76723162831feae83
saulokirchmaier <saulokirchmaier@gmail.com>
2021-02-12 15:41:02 -0300

    Ex. 6

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,7 +4,9 @@ import React from 'react';
 class MovieCard extends React.Component {
   render() {
     return (
-      <div>{this.props.data}</div>
+      <div>{this.props.movie.title}</div>
     );
   }
 }
+
+export default MovieCard;
