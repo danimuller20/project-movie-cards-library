8e5119e65edcd8fa43fb7e8bf6b5dc67ba06e782
J.Henrique <jhenriqueribeirodesa@gmail.com>
2021-02-12 18:19:30 -0300

    Requisito 6 e 7

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
     return (
-      <div />
+      <div>
+        <img src={ movie.imagePath } alt="Texto Alternativo" />
+      </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
