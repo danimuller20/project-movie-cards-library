02f47f0cb0cf150bb291320aae7e8c870f30a536
edison-ps <edison@osite.com.br>
2021-02-12 16:01:31 -0300

    Header V1.00

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,16 @@
-// Iniciando o projeto
+import React, { Component } from 'react';
+
+
+class Header extends Component {
+  render () {
+    return (
+      <div>
+        <header>
+            <h1>Movie Cards Library</h1>
+        </header>
+      </div>
+    )
+  }
+}
+
+export default Header;
