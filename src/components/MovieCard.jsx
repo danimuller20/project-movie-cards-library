03e4405acc8c079947e10b92c4aafa3b03182b02
RodrigoNeoEx <draja.acc@gmail.com>
2021-02-12 17:30:19 -0300

    done MovieList

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,13 @@
-// implement MovieCard component here
+import React from 'react'
+// import Data from '../data'
+
+const MovieCard = ({ movie }) => (
+  <div>
+    <h4>{movie.title}</h4>
+    <h5>{movie.subtitle}</h5>
+    <p>{movie.storyline}</p>
+    <img src={movie.imagePath} />
+  </div>
+);
+
+export default MovieCard
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard'
+// import movies from '../data'
+
+const MovieList = ({ movies }) => (
+  <div>
+  {movies.map((movie) => (
+    <MovieCard key={movie.title} movie={movie} />
+  ))}
+  </div>);
+
+
+
+export default MovieList;
