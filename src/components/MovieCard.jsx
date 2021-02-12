660bc459671e742da46daab3fe3d4af7442b9164
Layo Kaminski <layokaminski@gmail.com>
2021-02-12 16:50:00 -0300

    Ajustes do defeaultProps

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -28,11 +28,11 @@ MovieCard.propTypes = {
 };
 
 MovieCard.defaultProps = {
-  title: 'Kingsglaive',
-  subtitle: 'Final Fantasy XV',
-  storyline: 'King Regis, who oversees the land of Lucis',
+  title: 'Movie Title 1',
+  subtitle: 'Movie Subtitle 1',
+  storyline: 'Movie Storyline 1',
   rating: 4.5,
-  imagePath: 'images/Kingsglaive_Final_Fantasy_XV.jpg',
+  imagePath: 'images/movie_1',
 };
 
 export default MovieCard;
