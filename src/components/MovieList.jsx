a890d59321e39d5aa1eab69e16142bbcdec2cdc1
Marcelo Campos <marcelo.campos.economista@gmail.com>
2021-02-12 15:00:12 -0300

    Requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,11 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <p>Aqui ira aparecer a lista de filmes</p>
+    );
+  }
+}
+
+export default MovieList;
