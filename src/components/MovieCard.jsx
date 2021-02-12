2e36e3f33bbf50ce9120bca2ef1772f419132269
Matheus Martino <matheus.martino97@gmail.com>
2021-02-12 17:36:36 -0300

    Rendering MovieList.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,19 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+
+class MovieCard extends Component {
+  render() {
+    return (
+      <div>
+        <img src={this.props.movie.imagePath} alt="Banner do filme"/>
+        <div className='movie-info'>
+          <h2>{this.props.movie.title}</h2>
+          <h3>{this.props.movie.subtitle}</h3>
+          <p>{this.props.movie.storyline}</p>
+        </div>
+        <div>{this.props.movie.rating}</div>
+      </div>
+    );
+  }
+}
+
+export default MovieCard
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,27 @@
-// implement MovieList component here
+import React, { Component } from 'react'
+import movies from '../data'
+import MovieCard from './MovieCard'
+
+class MovieList extends Component {
+  render() {
+    // const cards = movies.map((movie, index) => {
+    //   return(
+    //       <MovieCard key={index} movie={movie}/>
+    //   )
+    // })
+
+    return(
+      <div>
+        {
+          movies.map((movie, index) => {
+            return(
+                <MovieCard key={`Movie Title ${index + 1}`} movie={movie}/>
+            )
+          })
+        }
+      </div>
+    )
+  }
+}
+
+export default MovieList
