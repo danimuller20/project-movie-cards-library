c6e57a6a6431d7498edd58cc97407d02a1e3ddd8
Christopher <chris.tryber@gmail.com>
2021-02-12 16:31:32 -0300

    Criei os componentes MovieCard e MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      <MovieCard />
+    );
+  }
+}
+
+export default MovieList;
