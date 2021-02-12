d094bbea3bbabab0ae8e7a11d8d2a1a2346e0fe4
J.Henrique <jhenriqueribeirodesa@gmail.com>
2021-02-12 19:03:51 -0300

    Adiciona css e organiza estruturas

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,7 +6,7 @@ export default class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
-      <ul>
+      <ul className="movie-list">
         { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </ul>
     );
