c3b0305f22e39fdf8f5928be565ff387306db74f
brunamfguimaraes <brunamfguimaraes@gmail.com>
2021-02-12 19:50:02 -0300

    Corrigindo erros

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,17 +5,17 @@ class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
     const { title, subtitle, storyline, imagePath, rating } = movie;
-      return (
-    <div>
-      <h4>{ title }</h4>
+    return (
+      <div>
+        <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
         <span>
-        <img src={ imagePath } alt="cardImage" />
-          </span>
-        <Rating rating={ rating } />
-    </div>
-  );
+          <img src={ imagePath } alt="cardImage" />
+        </span>
+      <Rating rating={ rating } />
+      </div>
+    );
   }
 }
 
