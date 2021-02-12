8f3ac64586a90da1408ea8e02ff30fdfbd69d44e
MarcioFujita <hanker78@gmail.com>
2021-02-12 15:59:42 -0300

    Requisito 32 V1.

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,12 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+    render() {
+        return (
+            <p>lista de filmes</p>
+        );
+    }
+}
+
+export default MovieList;
