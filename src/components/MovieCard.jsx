639d5d5d0d40435ef826f6a4b0484920475fb714
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-02-12 16:25:30 -0300

    ESLint - this.props correction

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
-    const { subtitle, storyline, rating, imagePath } = this.props.movie;
-    const title = this.props.movie.title;
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath } = movie;
+    /* const { title } = this.props; */
     return (
       <div className="movie-card">
         <img src={ imagePath } alt={ title } className="movie-card-image" />
