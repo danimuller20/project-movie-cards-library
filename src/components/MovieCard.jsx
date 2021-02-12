40e0954baef445cd9b148c87cad32649525dad12
brunamfguimaraes <brunamfguimaraes@gmail.com>
2021-02-12 18:28:43 -0300

    Adicionando requisitos de 6 a 11

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,23 @@
+import { render } from 'enzyme';
 import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+const { movie } = this.props;
+const { title, subtitle, storyline, imagePath, rating } = movie
+  return (
+    <div>
+      <h4>{ title }</h4>
+      <h5>{ subtitle }</h5>
+      <p>{ storyline }</p>
+      <span>
+        <img src={ imagePath } alt='cardImage'/>
+      </span>
+    </div>
+  )
 
-  }
+ }
 }
+
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,10 +3,10 @@ import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const movies = this.props.movies;
+    const { movies }= this.props;
     return (
       <div>
-        { movies.map((movie) =><MovieCard key={ movie.title } movie={ movie } />)}
+        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
   }
