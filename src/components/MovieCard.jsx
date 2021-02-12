1071169f90068d541a5258eb45aa0c9ea757fced
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-02-12 17:59:24 -0300

    Add class movie-card-body

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,10 +8,12 @@ class MovieCard extends React.Component {
     const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <div className="movie-card">
-        <img src={ imagePath } alt={ title } className="movie-card-image" />
-        <h4 className="movie-card-title">{ title }</h4>
-        <h5 className="movie-card-subtitle">{ subtitle }</h5>
-        <p className="movie-card-storyline">{ storyline }</p>
+        <div className="movie-card-body">
+          <img src={ imagePath } alt={ title } className="movie-card-image" />
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
+        </div>
         <div className="movie-card-rating">
           <Rating rating={ parseFloat(rating) } />
         </div>
