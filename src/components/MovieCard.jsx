d091d43ac8bb082c2e17ac74d0c14cf30c2ba92b
RodrigoNeoEx <draja.acc@gmail.com>
2021-02-12 18:34:32 -0300

    climate1

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,7 +6,7 @@ const MovieCard = ({ movie }) => (
     <h4>{ movie.title }</h4>
     <h5>{ movie.subtitle }</h5>
     <p>{ movie.storyline }</p>
-    <img src={ movie.imagePath } alt='movieImage' />
+    <img src={ movie.imagePath } alt="movieImage" />
     <Rating rating={ movie.rating } />
   </div>
 );
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import MovieCard from './MovieCard';
 
-
 const MovieList = ({ movies }) => (
   <div>
     {movies.map((movie) => (
