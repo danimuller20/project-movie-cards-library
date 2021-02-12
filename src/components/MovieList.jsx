150e5d0cd64eebbfbee9fa8e9e6e627aaeef1f4e
Layo Kaminski <layokaminski@gmail.com>
2021-02-12 15:32:13 -0300

    Requisitos 3, 4, 6, 7, 8, 9, 10, 11, 12, 14, 15 e 16

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.movies.map((movie, index) => <MovieCard key={index} movie={movie} />)};
+      </div>
+    );
+  }
+}
+
+export default MovieList;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,12 @@
 // implement Rating component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <p className="rating">{this.props.rating}</p>
+    );
+  }
+}
+
+export default MovieList;
