2982e65309eb759be3f8b3c66de22ddff67f9972
Hudson Carlos <hc88806@gmail.com>
2021-02-12 18:23:42 -0400

    correcting errors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 // implement Header component here
 import React from 'react';
 
-class Header extends React.Component {
+class Header extends React.Component(mov) {
   render() {
     return (
       <header><h1>Movie Cards Library</h1></header>
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,12 @@
 // implement MovieList component here
+import React from 'react';
+
+class MoviesList extends React.Component {
+  render() {
+    return (
+      <h1>hudson</h1>
+    );
+  }
+}
+
+export default MoviesList;
