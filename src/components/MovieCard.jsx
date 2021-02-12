f5df7ee08a6ef88f6ed8baf8bf25a08f2bed9ab0
MarcioFujita <hanker78@gmail.com>
2021-02-12 20:09:09 -0300

    mais classNames

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,12 +8,16 @@ class MovieCard extends React.Component {
     const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
     return (
       <div className="movie-list">
-        <div className="movie-card">    
-          <img src={ imagePath } alt={ title } />
-          <h4>{ title }</h4>
-          <h5>{ subtitle }</h5>
-          <p>{ storyline }</p>
-          <Rating rating={ rating } />
+        <div className="movie-card">
+          <div className="movie-card-body">
+            <img className="movie-card-image" src={ imagePath } alt={ title } />
+            <h4 className="movie-card-title">{ title }</h4>
+            <h5 className="movie-card-subtitle">{ subtitle }</h5>
+            <p className="movie-card-storyline">{ storyline }</p>
+            <div className="movie-card-rating">
+              <Rating rating={ rating } />
+            </div>
+          </div>
         </div>
       </div>
     );
