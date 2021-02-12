e2e45bcee37aaf2358230f982578a2726ac5c962
Osvaldo Borges <netoo70@gmail.com>
2021-02-12 14:59:48 -0300

    Requisito 03

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 class MovieList extends React.Component {
   render() {
     return (
-      <div>{this.props.movies}</div>
+      <div>
+        {this.props.movies.map(element => <p>{element.title}</p> )}
+      </div>
     );
   }
 }
