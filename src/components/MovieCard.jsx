6ad0384b378b74be647b55ec32472501f669fb79
MarcioFujita <hanker78@gmail.com>
2021-02-12 18:12:50 -0300

    Tentando arrumar MovieCard V2.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -10,7 +10,7 @@ class MovieCard extends React.Component {
 
     return (
       <div>
-        <img src={ imagePath } alt={`${ title } cover`} />
+        <img src={ imagePath } alt={ title } />
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
