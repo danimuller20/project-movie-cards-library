cf8a4a5141fcff4b988c94c9506c76890d216dd5
Carine Becker <caarine.m@gmail.com>
2021-02-12 18:25:24 -0300

    desestrutura this.props

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import Rating from './Rating'
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <div>
-        <img src={ imagePath } />
+        <img src={ imagePath } alt={title} />
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
