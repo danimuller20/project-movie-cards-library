52d4618b70a9ab826ddc09d2ed47b15e08b7796b
Rafa Reis <rafaelrnascimento2006@gmail.com>
2021-02-12 19:56:18 -0300

    estrutura do app feita

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import movies from '../data';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props
     return (
-      <div>
-        {this.props.movies.map(movie => <MovieCard key={movie.title} movie={movie}/>)}
-      </div>
-    )
+      <section className="movie-list">
+        { movies.map(movie => <MovieCard key={ movie.title } movie={ movie } />) }
+      </section>
+    );
   }
 }
 
