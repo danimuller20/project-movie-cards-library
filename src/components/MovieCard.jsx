6adc852141a8df44a9c77b441aeabcf239881045
johnealves <johne_af@hotmail.com>
2021-02-12 16:16:34 -0300

    Project Movie card library

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,30 @@
 // implement MovieCard component here
+import React, { Component } from 'react';
+import Rating from './Rating';
+import PropTypes from 'prop-types';
+import movies from '../data'
+
+class MovieCard extends Component {
+  render() {
+    // const { rating } = this.props.movies
+    return (
+      <div>
+        <img src={ this.props.movies.imagePath }/> 
+        <h4>{this.props.movies.title}</h4> 
+        <h5>{this.props.movies.subtitle}</h5>
+        <p>{ this.props.movies.storyline }</p>
+        <Rating rating={this.props.movies.rating}/>
+      </div>
+    )
+  }
+}
+
+MovieCard.propTypes = {
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  storyline: PropTypes.string,
+  rating: PropTypes.number,
+  imagePath: PropTypes.string
+}
+
+export default MovieCard
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,22 @@
 // implement MovieList component here
+// import movies from '../data'
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
+import PropTypes from 'prop-types';
+
+class MovieList extends Component {
+  render() {
+    const movies = this.props.movies
+    return (
+      movies.map((movie) => {
+        return <MovieCard movies={movie} key={movie.title}/>
+      })
+    )
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.array
+}
+
+export default MovieList;
