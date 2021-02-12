33960d3fba7c6f1e70ee3147237b93f7be298b4e
Rafael Pufal <rafael.pufal23@gmail.com>
2021-02-12 19:42:19 -0300

    Testando lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,21 +1,33 @@
 // implement MovieCard component here
 import React from 'react';
+import PropTypes from 'prop-types';
 import Rating from './Rating';
 
 class MovieCard extends React.Component {
-    render () {
-        const  { movie } = this.props;
-        const { title, subtitle, storyline, rating, imagePath } = movie;
-        return (
-            <div>
-                <img src={ imagePath } alt='Movie Poster'/>
-                <h4>{ title }</h4>
-                <h5>{ subtitle }</h5>
-                <p>{ storyline }</p>
-                <Rating rating={rating}/>
-            </div>
-        )
-    }
+  render() {
+    const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
+    return (
+      <div>
+        <img src={ imagePath } alt="Movie Poster" />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        <Rating rating={ rating } />
+      </div>
+    );
+  }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.objectOf(
+    PropTypes.object({
+      title: PropTypes.string,
+      subtitle: Proptypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: Proptypes.string,
+    }),
+  ).isRequired,
+};
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,16 +1,25 @@
 // implement MovieList component here
 import React from 'react';
-import MovieCard from './MovieCard'
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
-    render () {
-        const { movies } = this.props;
-        return (
-            <div className=''>
-                {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
-            </div>
-        )
-    }
+  render() {
+    const { movies } = this.props;
+    return (
+      <div className="">
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      </div>
+    );
+  }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.objectOf,
+};
+
+MovieList.defaultProps = {
+  movies: {},
+};
 
 export default MovieList;
