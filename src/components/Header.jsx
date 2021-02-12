6a368ba8c701db019e5350df14a4508dcb9e5665
Enio Nicael <enionicael@gmail.com>
2021-02-12 15:49:40 -0300

    Requisitos 1 e 2 cria um componete Header e renderiza na tela.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,11 @@
-// implement Header component here
-// Iniciando o projeto 🚀
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    <header>
+      <h1>Movie Cards Library</h1>
+    </header>
+  }
+}
+
+export default Header;
