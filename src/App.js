10162c4237a81ed240a6f8477cdd45c1187d786d
Thiago Granville <thiagogranville@gmail.com>
2021-02-12 18:24:40 -0300

    Requisito 17: Adiciona PropTypes a todos os componentes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <main className="movie-list">
       <MovieList movies={ data } />
+      </main>
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import MovieCard from './MovieCard';
+import PropTypes from 'prop-types';
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
-    return movies.map((movieData, index) => (
-      <MovieCard movie={ movieData } key={ index } />
+    return movies.map((movieData) => (
+      <MovieCard movie={ movieData } key={ movieData.title } />
     ));
   }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.array, 
 }
 
 export default MovieList;
