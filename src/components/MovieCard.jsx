926852dc82986729b2073ce9c84471932ca592aa
Matheus Martino <matheus.martino97@gmail.com>
2021-02-12 18:59:15 -0300

    Modifying the logic o the MovieList received the movies array as a prop.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
 import Rating from './Rating';
+import PropTypes from 'prop-types'
 
 class MovieCard extends Component {
   render() {
+    const { imagePath, title, subtitle, storyline, rating } = this.props.movie
     return (
       <div className="movie-card">
         <img
           className="movie-card-image"
-          src={ this.props.movie.imagePath }
+          src={ imagePath }
           alt="Banner do filme"
         />
         <div className="movie-card-body">
-          <h4 className="movie-card-title">{ this.props.movie.title }</h4>
-          <h5 className="movie-card-subtitle">{ this.props.movie.subtitle }</h5>
-          <p className="movie-card-storyline">{ this.props.movie.storyline }</p>
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
         </div>
-        <Rating rating={ this.props.movie.rating } />
+        <Rating rating={ rating } />
       </div>
     );
   }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.object.isRequired,
 }
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import movies from '../data';
 import MovieCard from './MovieCard';
 
 class MovieList extends Component {
   render() {
-    const cards = movies.map((movie, index) => (
-      <MovieCard key={ index } movie={ movie } />
+    const cards = this.props.movies.map((movie, index) => (
+      <MovieCard key={ `Movie Title ${(index + 1)}` } movie={ movie } />
     ));
 
     return <div className="movie-list">{ cards }</div>;
