ef4e3e253b960b6288759f6543bc20ad420e37e2
Hudson Carlos <hc88806@gmail.com>
2021-02-12 18:41:17 -0400

    correcting errors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 // implement Header component here
 import React from 'react';
 
-class Header extends React.Component(mov) {
+class Header extends React.Component {
   render() {
     return (
       <header><h1>Movie Cards Library</h1></header>
