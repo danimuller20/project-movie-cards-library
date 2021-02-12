38a3e19c358689c1eba9523fa56f3245f91b3a27
lhfmworker <lhfmworker@gmail.com>
2021-02-12 16:00:41 -0300

    req 4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,10 @@
 // implement MovieCard component here
+import React, { Component } from 'react';
+
+class MovieCard extends Component {
+  render() {
+    return (<p>moviecard: oi eu aqui</p>);
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,15 @@
 // implement MovieList component here
 import React, { Component } from 'react';
+import MovieCard from './MovieCard';
+import data from '../data';
 
 class MovieList extends Component {
   render() {
-    return (<p>movielist: oi eu aqui</p>);
+    return (
+      <div>
+        <p>movielist: oi eu aqui</p>
+        <MovieCard />
+      </div>);
   }
 }
 
