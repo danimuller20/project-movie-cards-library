8a373bfe5c242fd31a0514aadf0e5860e4694df6
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-02-12 17:22:03 -0300

    Evaluator Job is dead

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, imagePath } = movie;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <div className="movie-card">
         <img src={ imagePath } alt={ title } className="movie-card-image" />
         <h4 className="movie-card-title">{ title }</h4>
         <h5 className="movie-card-subtitle">{ subtitle }</h5>
         <p className="movie-card-storyline">{ storyline }</p>
+        <div className="movie-card-rating">
+          <Rating rating={ parseFloat(rating) } />
+        </div>
       </div>
     );
   }
@@ -22,6 +26,7 @@ MovieCard.propTypes = {
     subtitle: PropTypes.string,
     storyline: PropTypes.string,
     imagePath: PropTypes.string,
+    rating: PropTypes.number,
   }).isRequired,
 };
 
