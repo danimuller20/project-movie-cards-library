47ce4378c378e42b922ce2b845250a0087b2e430
danielfasanaro <danielfasanaro@gmail.com>
2021-02-12 16:43:08 -0300

    14th Requiriment: Add prop rating to Rating on MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,13 +3,14 @@ import Rating from './Rating'
 
 class MovieCard extends React.Component {
   render() {
+    const { imagePath, title, subtitle, storyline, rating } = this.props.movie;
     return (
     <div>
-      <img src={this.props.movie.imagePath}/>
-      <h4>{this.props.movie.title}</h4>
-      <h5>{this.props.movie.subtitle}</h5>
-      <p>{this.props.movie.storyline}</p>
-      <Rating />
+      <img src={imagePath}/>
+      <h4>{title}</h4>
+      <h5>{subtitle}</h5>
+      <p>{storyline}</p>
+      <Rating rating={rating}/>
     </div>
     )
   }
