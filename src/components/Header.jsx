0c4875741290eb9ad0961577e67c4a1a4152bd40
Qu4k3r <neves0007@gmail.com>
2021-02-12 15:22:52 -0300

    Dependencias instaladas | Feito componente Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,11 @@
 // implement Header component here
+import { createElement, Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return createElement('header', null,
+      createElement('h1', null, 'Movie Cards Library'));
+  }
+}
+
+export default Header;
