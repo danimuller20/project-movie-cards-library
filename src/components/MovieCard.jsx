9fa0a23bbbb44da9e0a12f3a681a3060a1fe8dd0
Bernardo Smith <bernardostech@gmail.com>
2021-02-12 16:54:53 -0500

    Quesito numero 1 ajustes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,16 @@
 // implement MovieCard component here
+import MovieList from '/components/MovieList';
+import React from 'react';
+import movies from './data';
+
+class MovieCard extends React.Component {
+    render() {
+        return (
+        <header>
+        <h1></h1>
+        </header>
+        )
+    } 
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
 // implement MovieList component here
+import React from 'react';
+import movies from './data';
+
+class MovieList extends React.Component {
+    render() {
+        return (
+        <header>
+        <h1></h1>
+        </header>
+        )
+    } 
+}
+
+export default MovieList;
