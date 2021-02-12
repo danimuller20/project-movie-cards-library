3442105686eb43fd54108a1bc8395ebfb0a1f7ae
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-02-12 18:14:46 -0300

    Lesson: Segundo requisito

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,10 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <h1></h1>
-    )
+    <header>
+      <h1>Movie Cards Library</h1>
+    </header>
+    );
   }
 }
 
