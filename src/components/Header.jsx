38703ca85e6c769d31a016fd6035acfa76c0fc99
Jose Daniel Arreaza <jdaniell.93@gmail.com>
2021-02-12 19:52:37 -0300

    movie card project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,15 @@
 // implement Header component here
-a
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1 className="movie-card-header">Movie Cards Library</h1>
+      </header>
+
+    );
+  }
+}
+
+export default Header;
