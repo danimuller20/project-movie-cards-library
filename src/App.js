79ff7750e012afa576406cdb370c51578857ddb5
Lucas Silva <martssilvapro@gmail.com>
2021-02-12 15:23:38 -0300

    resolvendo issues

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './components/Header';
 import './components/MovieList';
 import './components/MovieCard';
-import './components/Rating'
+import './components/Rating';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-
-
+      <Header />
     </div>
   );
 }
