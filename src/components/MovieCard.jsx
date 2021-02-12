d17df55322bf3193c951e6c104161c4c63d521d4
Marcelo Campos <marcelo.campos.economista@gmail.com>
2021-02-12 16:42:21 -0300

    Fix some esLint issues

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,7 +8,7 @@ class MovieCard extends React.Component {
     const { title, subtitle, storyline, rating, imagePath } = movie;
     return (
       <div>
-        <img src={ imagePath } />
+        <img src={ imagePath } alt={ title } />
         <div>
           <h4>{ title }</h4>
           <h5>{ subtitle }</h5>
