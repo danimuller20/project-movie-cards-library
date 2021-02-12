ef6d984c7ede9f3df59c43949226f4cc8a158aee
BBrolesi <brunohsbrolesi@gmail.com>
2021-02-12 17:48:13 -0300

    Implement Rating Component

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,12 @@
-// implement Rating component here
+import React, { Component } from 'react'
+
+class Rating extends Component{
+    render(){
+        return(
+            <div>
+                <h4>Rating</h4>
+                <h5>{this.props.rating}</h5>
+            </div>
+        );
+    }
+}
