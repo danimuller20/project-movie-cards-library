b32703b3cd061d3a849dbca2218ef12a9c247036
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-02-12 15:49:59 -0300

    Rating renderizado

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import Rating from './Rating';
 
 class MovieCard extends Component{
   render() {
     return (
       <div>
-        <img src={this.props.movie.imagePath}/>
+        <img src={this.props.movie.imagePath} alt={`${this.props.movie.title}'s image`}/>
         <h4>{this.props.movie.title}</h4>
         <h5>{this.props.movie.subtitle}</h5>
         <p>{this.props.movie.storyline}</p>
+        <Rating />
       </div>
     )
   }
