b0a4a58b1c23b1d11e223b2d7e65d9928b937ac2
Pedro Henrique <pedro.876.ph@gmail.com>
2021-02-12 16:03:35 -0300

    commitao bomba 3 ao 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,21 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+
+    render() {
+
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
+
+    return (
+      <div className="movie-card">
+        <img src={imagePath}></img>
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      (this.props.movies.map(((movie) => <MovieCard movie={movie} key={movie.title} />)))
+    );
+  }
+}
+
+export default MovieList;
