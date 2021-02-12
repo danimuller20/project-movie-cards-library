e258524716c2f50cd309f55a4d30fe23dc12807d
Carine Becker <caarine.m@gmail.com>
2021-02-12 19:35:30 -0300

    fixed eslinter issues

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
@@ -13,7 +14,7 @@ class MovieList extends React.Component {
 }
 
 MovieList.propTypes = {
-  movies: {},
-}
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
