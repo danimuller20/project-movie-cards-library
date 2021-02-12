0b6b725929bfd0e472f6be1c4ea6be23b134adb8
LucioOSilva <lucioosilva00@gmail.com>
2021-02-12 14:55:17 -0300

    added React Component- Rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,10 @@
 // implement Rating component here
+import React from 'react';
+
+class Rating extends React.Component {
+  render() {
+    return <p>Rating <span className="rating">{this.props.rating}</span></p>
+  }
+}
+
+export default Rating;
