1340a98b2d1edf533bd63016a91c2b6f154f1720
Christopher <chris.tryber@gmail.com>
2021-02-12 18:13:04 -0300

    Requisito 6 terceira tentativa

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
-    // const { key, movie } = this.props;
+    const { key, movie } = this.props;
     return (
       <span />
     );
