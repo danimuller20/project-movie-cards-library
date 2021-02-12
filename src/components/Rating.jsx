bc527572e541a6f3326d201f4ff494bec0e74f3f
Matheus Martino <matheus.martino97@gmail.com>
2021-02-12 18:33:33 -0300

    Fixing code climate issues.

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 class Rating extends Component {
-  
   render() {
-    const { rating } = this.props 
+    const { rating } = this.props;
 
     return (
       <div className="movie-card-rating">
@@ -15,7 +14,7 @@ class Rating extends Component {
 }
 
 Rating.propTypes = {
-  rating: PropTypes.string,
-}
+  rating: PropTypes.string.isRequired,
+};
 
 export default Rating;
