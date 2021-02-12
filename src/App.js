f2f592bdbf30372b902086bc5423f59ca8087c0b
Enio Nicael <enionicael@gmail.com>
2021-02-12 16:09:53 -0300

    Requisito 3 cria um componente MovieList.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+
+import data from '../src/data';
+
 import Header from './components/Header';
-// import Header from '../src/components/Header';
+import MovieList from './components/MovieCard';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList movies={data[0]} />
     </div>
   );
 }
