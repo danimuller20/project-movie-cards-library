5610f41c79b608c4796a47107d121c1c3eb72b52
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2021-02-12 17:15:55 -0300

    Primeiro Requisito

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,12 +6,12 @@ import PropTypes from 'prop-types';
 
 class Header extends React.Component {
   render() {
-      return (
-          <header>
-              <h1>Movie Cards Library</h1>
-          </header>  
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>  
       );
-  }
+    }
 }
 
 // Header.propTypes = {
