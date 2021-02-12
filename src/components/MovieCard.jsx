7fafb9db3e2accb196950b0170b210c4861d1cce
marcusWittho <witthoeft.mcs@hotmail.com>
2021-02-12 20:15:48 -0300

    Falta o requisito 17

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import Rating from '../components/Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
     return (
       <div>
-        <img src={ imagePath } alt={ title }/>
+        <img src={ imagePath } alt={ title } />
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
+        <Rating rating={ rating }/>
       </div>
-    )
+    );
   }
 }
 
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,9 +5,10 @@ class MovieList extends React.Component {
   render() {
     return (
       <div>
-        {this.props.movies.map(item => <MovieCard movie={ item } key={ item.title }/>)};
+        {this.props.movies.map((item) =>
+          <MovieCard movie={ item } key={ item.title }/>)};
       </div>
-    )
+    );
   }
 }
 
