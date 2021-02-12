c24c98aa18bbd472629c533ece0b2a9aa295e2ef
BernardoFontes <bfontes.1997@gmail.com>
2021-02-12 17:15:59 -0300

    code climate

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import MovieCard from './MovieCard'
+import MovieCard from './MovieCard';
 import PropTypes from 'prop-types';
 
-class MovieList extends React.Component{
-    render(){
-        const { movies } = this.props;
-        return(
-            <div className = 'movie-list'>
-                {movies.map((movie) => <MovieCard key = { movie.title } movie = { movie } />)}
-            </div>
-        );
-}
+class MovieList extends React.Component {
+  render(){
+    const { movies } = this.props;
+    return (
+      <div className='movie-list'>
+        {movies.map((movie) => <MovieCard key = { movie.title } movie = { movie } />)}
+      </div>
+    );
+  };
 }
 
 MovieList.propTypes = { movies: PropTypes.arrayOf.isRequired };
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Rating extends React.Component{
-    render(){
-        const { rating } = this.props
-        return (
-            <div>
-                <p className='rating'>{rating}</p>
-            </div>
-        )
-    }
+class Rating extends React.Component {
+  render(){
+    const { rating } = this.props;
+    return (
+      <div>
+      <p className='rating'>{rating}</p>
+      </div>
+    );
+  }
 }
 
 Rating.propTypes = { rating: PropTypes.number.isRequired };
