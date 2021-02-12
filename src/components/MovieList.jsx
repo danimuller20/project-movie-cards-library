729f3c52aa712a7fb48199124fe4fc0c6f3e77c9
saulokirchmaier <saulokirchmaier@gmail.com>
2021-02-12 15:38:53 -0300

    Ex. 3/4/5 moviecar and movielist

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,16 @@
 // implement MovieList component here
 import React from 'react';
+import MovieCard from 'src/components/MovieCard.jsx';
 
 class MovieList extends React.Component {
   render() {
+    const movies = this.props.movies;
     return (
-
+      <div>
+        {movies.map(movie => <MovieCard data={movie} key={movie.title}/>)}
+      </div>
     );
-  };
+  }
 }
+
+export default MovieList;
