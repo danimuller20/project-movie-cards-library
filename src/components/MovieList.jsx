651fac7ea730e91ad89565198f1335307b9338d9
marcusWittho <witthoeft.mcs@hotmail.com>
2021-02-12 18:43:41 -0300

    Esqueci de comitargit add . Aqui vão os 10 primeiros requisitos

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.movies.map(item => <MovieCard movie={ item } key={ item.title }/>)};
+      </div>
+    )
+  }
+}
+
+export default MovieList;
