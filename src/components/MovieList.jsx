1f6b9f4be57573e136603c25f0c2476d54bdcd3f
Rafa Reis <rafaelrnascimento2006@gmail.com>
2021-02-12 20:13:02 -0300

    corrigindo issues do codeclimate

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,10 +3,10 @@ import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const { movies } = this.props
+    const { movies } = this.props;
     return (
       <section className="movie-list">
-        { movies.map(movie => <MovieCard key={ movie.title } movie={ movie } />) }
+        { movies.map(movie => ( <MovieCard key={ movie.title } movie={ movie } /> )) }
       </section>
     );
   }
