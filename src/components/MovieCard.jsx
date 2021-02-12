5166985d0ae40aaf39ed566277eecc9b95c66df5
danielfasanaro <danielfasanaro@gmail.com>
2021-02-12 16:29:36 -0300

    9th Requiriment: Render movie subtitle on MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,6 +6,7 @@ class MovieCard extends React.Component {
     <div>
       <img src={this.props.movie.imagePath}/>
       <h4>{this.props.movie.title}</h4>
+      <h5>{this.props.movie.subtitle}</h5>
     </div>
     )
   }
