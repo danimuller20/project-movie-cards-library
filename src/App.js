daa3f7afa961fe5ad00776ec8c72844cba0af467
Enio Nicael <enionicael@gmail.com>
2021-02-12 15:55:39 -0300

    Requisitos 1 e 2 correcoes do eslint.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import Header from '../src/components/Header';
+import Header from './components/Header';
+// import Header from '../src/components/Header';
 
 function App() {
   return (
