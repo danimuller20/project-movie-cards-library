ae98a6aed6eb8a39da3c16ce30a1d39d80a124f9
Marcelo Campos <marcelo.campos.economista@gmail.com>
2021-02-12 16:31:55 -0300

    Fix some esLint issues

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -26,6 +26,7 @@ MovieCard.propTypes = {
     subtitle: PropTypes.string,
     storyline: PropTypes.string,
     imagePath: PropTypes.string,
+    rating: PropTypes.number,
   }).isRequired,
 };
 
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -7,9 +7,9 @@ class MovieList extends React.Component {
     const { movies } = this.props;
     return (
       <div className="">
-        {movies.map(movie => (
-          <MovieCard key={ movie.title } movie={ movie } />
-        ))}
+        {movies.map((movie) => 
+        <MovieCard key={ movie.title } movie={ movie } />
+        )}
       </div>
     );
   }
