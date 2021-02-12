f13f4cf047a454d756c6c6a58ccf3c0d2df84cd6
danielfasanaro <danielfasanaro@gmail.com>
2021-02-12 17:03:23 -0300

    17th Requiriment: Add PropTypes to all components

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import MovieCard from './MovieCard'
+import PropTypes from 'prop-types'
 
 class MovieList extends React.Component {
   render() {
@@ -8,5 +9,17 @@ class MovieList extends React.Component {
     });
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string
+    })
+  )
+};
 
 export default MovieList;
