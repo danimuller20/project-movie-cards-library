b867be569de09f75c022a4f9d31cabab6cb4ecb0
MarcioFujita <hanker78@gmail.com>
2021-02-12 19:34:44 -0300

    Adicionando .css

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,13 +7,14 @@ class MovieCard extends React.Component {
   render() {
     const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
     return (
-      <div>
-        <img src={ imagePath } alt={ title } />
-        <h4>{ title }</h4>
-        <h5>{ subtitle }</h5>
-        <p>{ storyline }</p>
-        {/* <span>{ rating }</span> */}
-        <Rating rating={ rating } />
+      <div className="movie-list">
+        <div className="movie-card">    
+          <img src={ imagePath } alt={ title } />
+          <h4>{ title }</h4>
+          <h5>{ subtitle }</h5>
+          <p>{ storyline }</p>
+          <Rating rating={ rating } />
+        </div>
       </div>
     );
   }
