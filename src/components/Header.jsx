b6b10f8de07620b746f1afcf5cacf53009056956
Felipe Cardoso <felipedclc@gmail.com>
2021-02-12 15:49:20 -0300

    identacao

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,13 @@
 import React from 'react';
 
 class Header extends React.Component {
-    render() {
-        return (
-            <div className="header">
-                <header>{'Movie Cards Library'}</header>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="header">
+        <header>{ 'Movie Cards Library' }</header>
+      </div>
+    );
+  }
 }
 
 export default Header;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,21 +3,22 @@
 import React from 'react';
 
 class MovieList extends React.Component {
-    render() {
-            const { title, subtitle, storyline, rating, imagePath } = this.props.movies;
-        return (
-            <section>
-                <img src={imagePath} alt={`${title} image`}></img>
-                <div>
-                    <p className="title">{title}</p>
-                    <p className="subtitle">{subtitle}</p>
-                    <p className="storyline">{storyline}</p>
-                    <div className="rating">
-                        <p>{rating}</p>
-                    </div>
-                </div>
-            </section>
-        );
-    }
+  render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movies;
+      return (
+      <section>
+        <img src={ imagePath } alt={ `${title} image` }></img>
+        <div>
+          <p className="title">{title}</p>
+          <p className="subtitle">{subtitle}</p>
+          <p className="storyline">{storyline}</p>
+          <div className="rating">
+            <p>{rating}</p>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
+export default MovieList;
