e176870134659fbdcd217c635455eb7c5f842024
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-02-13 14:04:23 -0300

    Refatorando Header de component function para class

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
 // implement Header component here!
 import React from 'react';
 
-const Header = () => (
-  <header>
-    <h1>Movie Cards Library</h1>
-  </header>
-);
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    )
+  }
+}
 
 export default Header;
