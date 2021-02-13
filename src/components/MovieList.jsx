dc870905cc5b7b99d2f48815fc82d0c8b60ddba3
Micaela Alves <micaela.alves93@gmail.com>
2021-02-13 01:04:50 -0300

    Requirement 6 - add MovieCard component

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,18 @@
 // implement MovieList component here
 import React from 'react';
 
+import MovieCard from './MovieCard.jsx';
+
 class MovieList extends React.Component {
   render() {
-    const movies = this.props;
-    return {}
+    const movies = this.props.movies;
+    return (
+      <div>
+        {movies.forEach((element) => <MovieCard />)}
+      </div>
+    )
   }
 }
+
 
 export default MovieList;
