e0712be9938690c9e73f672dbb6615b75340aaa5
Natalia Negreiros <nataliaknegres@gmail.com>
2021-02-13 14:47:50 -0300

    add requirements 1-8

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,9 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return <img src="images/movie_1" alt="" />;
+  }
+}
+
+export default MovieCard;
