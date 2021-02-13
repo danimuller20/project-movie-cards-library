781508e6151832b2b9da1002d9c82946752aaaa3
Micaela Alves <micaela.alves93@gmail.com>
2021-02-12 21:16:02 -0300

    Requirement 3 - add MovieList component

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,10 +3,8 @@ import React from 'react';
 
 class MovieList extends React.Component {
   render() {
-    return
-      <div>
-        ${movies}
-      </div>
+    const movies = this.props;
+    return {}
   }
 }
 
