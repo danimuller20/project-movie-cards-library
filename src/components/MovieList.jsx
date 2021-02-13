1a0dee404c0698fec8b7abfd6e4f040a7b9f9ec5
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2021-02-13 12:34:05 -0300

    Segundo requisito

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 import Movies from './Movies';
 
 class MovieList extends React.Component {
-    render () {
-        return (
-            <div>
-                {this.props.data.map((movie, index) => <Movies movieInfo={movie} key={index} />)}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        {this.props.data.map
+        ((movie, index) => <Movies movieInfo={movie} key={index} />)} 
+      </div>
+    )
+  }
 }
 
 export default MovieList;
