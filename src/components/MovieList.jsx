5066a2ca1d2fea3e3227f82b237f7f87e46736d9
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-02-13 17:27:13 -0300

    PropTypes em MovieList, MovieCard e Rating

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,6 +1,7 @@
 // implement MovieList component here
 import React from 'react';
 import MovieCard from './MovieCard';
+import PropTypes from 'prop-types';
 
 class MovieList extends React.Component {
   render() {
@@ -9,6 +10,18 @@ class MovieList extends React.Component {
       return <MovieCard movie={ movie } key={ movie.title }/>
     });
   }
+}
+
+MovieList.propTypes = {
+  movie: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+    })
+  ),
 }
 
 export default MovieList;
