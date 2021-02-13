9c8f026ca0a830eea714900076f26d2004ae8769
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-02-13 17:42:07 -0300

    Ajustes finais2

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,22 +1,22 @@
 // implement MovieCard component here!
 import React from 'react';
-import Rating from './Rating';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, imagePath, rating }  = movie;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <div className="movie-card">
-        <img src={ imagePath } alt='Cartás do filme' />
+        <img src={ imagePath } alt="Cartás do filme" />
         <div>
           <h4>{ title }</h4>
           <h5>{ subtitle }</h5>
           <p>{ storyline }</p>
         </div>
         <div>
-            <Rating rating={ rating } />
+          <Rating rating={ rating } />
         </div>
       </div>
     );
