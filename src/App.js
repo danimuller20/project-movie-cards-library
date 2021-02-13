2ccfb078a546881c683ba81ab99ec466c4a5abee
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-02-13 04:06:31 -0300

    Evaluator nao rodou da ultima fez. Acrescentei linha vazia no final pra testar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Header from './components/Header';
 function App() {
   return (
     <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
       <Header />
       <MovieCard />
     </div>
