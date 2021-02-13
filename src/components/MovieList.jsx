93a366ee227cd1cf76f68fc5eaf1465be69d1a49
Nathália <nathaliaveneziano@icloud.com>
2021-02-13 18:42:04 -0300

    Projeto Finalizado

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,2 +1,17 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
 
+class MovieList extends React.Component{
+  render() {
+    return (
+      <main className="movie-list">
+        {this.props.movies.map((movie) => (
+          <MovieCard key={movie.title} movie={movie} />
+        ))}
+      </main>
+    );
+  }
+}
+
+export default MovieList;
