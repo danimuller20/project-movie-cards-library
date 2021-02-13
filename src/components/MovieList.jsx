bd40bf4e1254399189a3d3e15a0b738664338f9d
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-02-13 17:48:36 -0300

    Ajustando importaçao do PropTypes e refatorando map

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,14 +1,12 @@
 // implement MovieList component here
 import React from 'react';
-import MovieCard from './MovieCard';
 import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
-    return movies.map((movie) => {
-      return <MovieCard movie={ movie } key={ movie.title }/>
-    });
+    return movies.map((movie) => <MovieCard movie={ movie } key={ movie.title }/>);
   }
 }
 
