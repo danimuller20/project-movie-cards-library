1a27dc4c2d6bd78cde653e9ac4fded44f7c38c7e
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2021-02-13 12:23:38 -0300

    segundo requisito

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,31 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+    render () {
+        return (
+            <div>
+                {this.props.data.map((movie, index) => <Movies movieInfo={movie} key={index} />)}
+            </div>
+        )
+    }
+}
+
+class Movies extends React.Component {
+    render () {
+        const {title, subtitle, storyline, rating, imagePath} = this.props.movieInfo;
+        return (
+            <div>
+                <section>
+                    <img src={imagePath} alt="broken"></img>
+                    <p>{subtitle}</p>
+                    <p>{title}</p>
+                    <p>{storyline}</p>
+                    <p>{rating}</p>
+                </section>
+            </div>
+        )
+    }
+}
+
+export default MovieList;
