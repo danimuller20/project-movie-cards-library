51686466ceaa1f74a31988cec6e88e558c47d323
Bernardo Smith <bernardostech@gmail.com>
2021-02-13 14:26:34 -0500

    Ajustando as questoes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,13 +2,25 @@
 import MovieList from '/components/MovieList';
 import React from 'react';
 import movies from './data';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
     render() {
+        const { title, subtitle, storyline, imagePath, rating } = this.props.movies;
         return (
-        <header>
-        <h1></h1>
-        </header>
+        <div>
+          <div>
+            {this.props.movies.map(movie => movie = {movie})}
+          </div>
+          <div>
+          <img src={imagePath} alt={`${title} sprite`} />
+          </div>
+          <div>
+              <h4>Title: {title} </h4>
+              <h5>Subtitle: {subtitle} </h5>
+              <p>Sinpose: {sinopse} </p>
+          </div>
+        </div>
         )
     } 
 }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,14 @@
 // implement Rating component here
+import MovieCard from '/components/MovieCard';
+import React from 'react';
+
+
+class Rating extends React.Component {
+    render() {
+        
+        return (
+        )
+    } 
+}
+
+export default Rating;
