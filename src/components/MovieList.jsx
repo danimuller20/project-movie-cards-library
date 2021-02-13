fde4f97560720848440d69de67d9b139c8b68a5c
Enio Nicael <enionicael@gmail.com>
2021-02-13 00:31:48 -0300

    Correcoes do eslint.

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,7 +5,7 @@ import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const movies = this.props.movies;
+    const { movies } = this.props;
     const moviesList = movies
       .map((movie) => <MovieCard key={ movie.title } movie={ movie } />);
     return (
