ab3b7fa1fa4021d9072c098b2ed0ccfed1af6140
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-02-13 16:53:51 -0300

    Rating criado  e outras alteraçoes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,8 +1,25 @@
 // implement MovieCard component here!
 import React from 'react';
+import Rating from './Rating';
 
-const MovieCard = () => (
-  <h1>Teste</h1>
-);
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating }  = movie;
+    return (
+      <div className="movie-card">
+        <img src={ imagePath } alt='Cartás do filme' />
+        <div>
+          <h4>{ title }</h4>
+          <h5>{ subtitle }</h5>
+          <p>{ storyline }</p>
+        </div>
+        <div>
+            <Rating rating={ rating } />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,11 @@
 // implement MovieList component here
-import React, { Component } from 'react';
+import React from 'react';
 import MovieCard from './MovieCard';
 
-class MovieList extends Component {
+class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
-    return movies.map(movie => {
+    return movies.map((movie) => {
       return <MovieCard movie={ movie } key={ movie.title }/>
     });
   }
