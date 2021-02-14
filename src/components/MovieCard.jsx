3c68feeed127cc9a4fef0d60a95030cbf3fcdc50
Genadi Reuben <genadireuben91@gmail.com>
2021-02-14 20:51:58 -0300

    rating implemented

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-// import Rating from './Rating';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { card } = this.props;
     const { title, subtitle, storyline, imagePath, rating } = card;
     return (
-      <div>
-        <img src={ imagePath } alt="movie cards" />
-        <h4>{title}</h4>
-        <h5>{subtitle}</h5>
-        <p>{storyline}</p>
-        {/* <Rating rating={ rating } /> */}
+      <div className="movie-card">
+        <img className="movie-card-image" src={ imagePath } alt="movie cards" />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+          <Rating rating={rating} />
+        </div>
       </div>
     );
   }
