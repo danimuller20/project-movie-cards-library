ab9fd07388e432f0ba147119b6b26de4a78f2d54
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-02-14 01:36:52 -0300

    Ultimo requisito Rating nao passa. Tentando desvendar o misterio

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -15,9 +15,7 @@ class MovieCard extends React.Component {
           <h5 className="movie-card-subtitle">{ subtitle }</h5>
           <p className="movie-card-storyline">{ storyline }</p>
         </div>
-        <div>
-          <Rating rating={ rating } />
-        </div>
+        <Rating rating={ rating } />
       </div>
     );
   }
