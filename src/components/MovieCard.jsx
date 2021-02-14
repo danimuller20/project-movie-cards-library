96073e0e5f725fe450ab1e792b7e9185f79dcb38
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-02-14 00:57:28 -0300

    Ajustes CSS

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -11,9 +11,9 @@ class MovieCard extends React.Component {
       <div className="movie-card movie-card-body">
         <img className="movie-card-image" src={ imagePath } alt="Cartás do filme" />
         <div className=''>
-          <h4>{ title }</h4>
-          <h5>{ subtitle }</h5>
-          <p>{ storyline }</p>
+          <h4 className='movie-card-title'>{ title }</h4>
+          <h5 className='movie-card-subtitle'>{ subtitle }</h5>
+          <p className='movie-card-storyline'>{ storyline }</p>
         </div>
         <div>
           <Rating rating={ rating } />
