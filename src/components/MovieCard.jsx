25bc031f62bf1d9720ce0bfaefb315812e24abaa
Pedro Henrique <pedroenrique.400@gmail.com>
2021-02-15 18:32:29 -0300

    Validando Eslint/9

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,17 +2,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Rating from './Rating';
+
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
     const { title, subtitle, storyline, rating, imagePath } = movie;
     return (
       <div className="movie-card">
-        <img src={ imagePath } alt='Imagem Do filme' className="movie-card-image"/>
+        <img src={ imagePath } alt="Imagem Do filme" className="movie-card-image"/>
         <h4 className="movie-card-title">{ title }</h4>
         <h5 className="movie-card-subtitle">{ subtitle }</h5>
         <p className="movie-card-storyline">{ storyline }</p>
-        {<Rating rating={ rating } />}
+        <Rating rating={ rating } />
       </div>
     );
   }
