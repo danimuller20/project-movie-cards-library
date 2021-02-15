d3d76d280bb7ab559bfcbc550ae80a3610568b3f
Tiago de Almeida Paz <tiagopaz.dev@gmail.com>
2021-02-15 11:26:58 -0300

    resolvendo erros

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import Data from './data';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const { movies } = this.props;
   }
 }
 
 export default MovieList;
-
