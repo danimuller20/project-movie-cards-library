9778dd1c369969dac5a39d57eeccba9462f5f4de
lhfmworker <lhfmworker@gmail.com>
2021-02-15 09:49:21 -0300

    troca de class para function no movielist

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,16 +1,26 @@
 // implement MovieList component here
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
-import data from '../data';
 
-class MovieList extends Component {
-  render() {
-    return (
-      <div>
-        {data.map((movieI) => <MovieCard key={ movieI.title } movie={ movieI } />)}
-      </div>
-    );
-  }
+const MovieList = ({ movies }) => {
+  return (
+    <div>
+      {movies.map((movieI) => <MovieCard key={ movieI.title } movie={ movieI } />)}
+    </div>
+  );
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.objectOf(
+    PropTypes.shape({
+      title: PropTypes.number,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      imagePath: PropTypes.imagePath,
+      rating: PropTypes.number,
+    }),
+  ).isRequired,
+};
 
 export default MovieList;
