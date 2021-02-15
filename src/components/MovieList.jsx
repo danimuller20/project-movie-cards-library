5551cb344b612625c127bc223390ca006623828b
lhfmworker <lhfmworker@gmail.com>
2021-02-15 09:53:16 -0300

    troca de class para function no movielist eslint fix

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,13 +3,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
-const MovieList = ({ movies }) => {
-  return (
-    <div>
-      {movies.map((movieI) => <MovieCard key={ movieI.title } movie={ movieI } />)}
-    </div>
-  );
-}
+const MovieList = ({ movies }) => (
+  <div>
+    {movies.map((movieI) => <MovieCard key={ movieI.title } movie={ movieI } />)}
+  </div>
+);
 
 MovieList.propTypes = {
   movies: PropTypes.objectOf(
