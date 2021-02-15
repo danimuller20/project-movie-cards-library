f861f64afc40ee72b787c6588d25c9165f73f473
Maximilian Kaden <mkadenfilgo@gmail.com>
2021-02-15 09:24:10 -0300

    Requisitos MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,20 @@
 // implement MovieCard component here
+import React from 'react'
+
+const MovieCard = (prop) => (
+  <div className="movie-card">
+    <img src={prop.movie.imagePath} className="movie-card-image"></img>
+    <div className="movie-card-body">
+      <h2 className="movie-card-title">{prop.movie.title}</h2>
+      <h3 className="movie-card-subtitle">{prop.movie.subtitle}</h3>
+      <p className="movie-card-storyline">{prop.movie.storyline}</p>
+    </div>
+    <div className="movie-card-rating">
+      <div className="rating">
+        <p>{prop.movie.rating}</p>
+      </div>
+    </div>
+  </div>
+)
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,11 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard'
+
+const MovieList = (prop) => (
+  <div className="movie-list">
+    {prop.movies.map((movie, index) => <MovieCard movie={movie} key={`Movie Title ${index + 1}`}/>)}
+  </div>
+)
+
+export default MovieList;
