c223fdd514c506829d50404bb1e090f8c88ed5a3
Renato Feitosa <pythaqua@gmail.com>
2021-02-15 19:44:44 -0300

    [feat] (Header) add 1st requirement

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,9 @@
-// implement Header component here
+import React from 'react';
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <header></header>
+    );
+  }
+}
