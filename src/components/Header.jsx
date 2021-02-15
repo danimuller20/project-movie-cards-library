c3d437ff7c57aed04616a170ed4d7bc651fcb957
Maximilian Kaden <mkadenfilgo@gmail.com>
2021-02-14 23:04:22 -0300

    Correçao ESlint

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@
 import React from 'react';
 
 const Header = () => (
-    <header className="movie-card-header">
-      <h1 className="page-title">Movie Cards Library</h1>
-    </header>
-  );
+  <header className="movie-card-header">
+    <h1 className="page-title">Movie Cards Library</h1>
+  </header>
+);
 
 export default Header;
