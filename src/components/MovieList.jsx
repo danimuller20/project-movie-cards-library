9e5459caae857ac25dd05d1940443267c60fd14d
Tiago de Almeida Paz <tiagopaz.dev@gmail.com>
2021-02-15 11:24:22 -0300

    requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,12 @@
-// implement MovieList component here
+import React from 'react';
+import Data from './data';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+  }
+}
+
+export default MovieList;
+
