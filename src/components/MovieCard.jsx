0b070512d3219031d237649b42e9ef67f9becb39
Maximilian Kaden <mkadenfilgo@gmail.com>
2021-02-15 12:30:29 -0300

    Requisitos 1 ao 16

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,17 @@
 // implement MovieCard component here
 import React from 'react';
+import Rating from './Rating'
 
 const MovieCard = (prop) => (
   <div className="movie-card">
     <img src={ prop.movie.imagePath } className="movie-card-image" alt="movie-poster" />
     <div className="movie-card-body">
-      <h2 className="movie-card-title">{ prop.movie.title }</h2>
-      <h3 className="movie-card-subtitle">{ prop.movie.subtitle }</h3>
+      <h4 className="movie-card-title">{ prop.movie.title }</h4>
+      <h5 className="movie-card-subtitle">{ prop.movie.subtitle }</h5>
       <p className="movie-card-storyline">{ prop.movie.storyline }</p>
     </div>
     <div className="movie-card-rating">
-      <div className="rating">
-        <p>{prop.movie.rating}</p>
-      </div>
+      <Rating rating={prop.movie.rating}/>
     </div>
   </div>
 );
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,10 @@
 // implement Rating component here
+import React from 'react'
+
+const Rating = (prop) => (
+  <div className="rating">
+    {prop.rating}
+  </div>
+)
+
+export default Rating;
