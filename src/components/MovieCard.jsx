12b55791bfc5ff9a4f2ca8af626c3522c0f601c7
diego-vini <analistamm@gmail.com>
2021-02-16 14:21:59 -0300

    Primeiros requisitos do projeto

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,23 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+    render() {
+        const {title, subtitle, storyLine, rating, imagePath} = this.props.cards
+return (
+    <div>
+        <p> { title } </p>
+        <p> { subtitle } </p>
+        <p> { storyLine } </p>
+        <img src= { imagePath } />
+        <div>
+            <p> { rating } </p>
+        </div>
+    </div>
+)
+    }
+
+};
+
+export default MovieCard;
+
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,18 @@
 // implement MovieList component here
+import React from 'react';
+import movies from '../data';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+    render() {
+         return (
+           <div>
+               {movies.map((movie, index) => {
+                   <MovieCard cards = { movie } key={ index } />
+               })}
+            </div>
+       )
+    }
+};
+
+export default MovieList;
