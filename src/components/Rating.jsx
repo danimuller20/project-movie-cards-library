846009a08595fbc8299e58c6a88a612080d93c75
Renato Feitosa <pythaqua@gmail.com>
2021-02-15 21:31:34 -0300

    [feat] (Rating) add 11th requirement

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,10 @@
-// implement Rating component here
+import React from 'react';
+
+export default class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <div />
+    );
+  }
+}
