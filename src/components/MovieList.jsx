fdd35c66ad5eb6d0d01e2f2964a6cca6dc7da740
Renato Feitosa <pythaqua@gmail.com>
2021-02-15 21:24:36 -0300

    [feat] (MovieList) add requirements 3 to 5

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+import movies from '../data';
+
+export default class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    const moviesList = movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />);
+    return (
+      <div>
+        { moviesList }
+      </div>
+    );
+  }
+}
