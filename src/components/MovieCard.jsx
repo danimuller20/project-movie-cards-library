9955f79371b66aec4ecf69a19a84d3eebafe6186
Renato Feitosa <pythaqua@gmail.com>
2021-02-15 21:25:41 -0300

    [tests] (movieList) enable tests

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,23 @@
-// implement MovieCard component here
+import React from 'react';
+
+export default class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    const {
+      title,
+      subtitle,
+      storyline,
+      rating,
+      imagePath ,
+    } = movie;
+
+    return (
+      <div>
+        <img src={ imagePath } />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+      </div>
+    );
+  }
+}
