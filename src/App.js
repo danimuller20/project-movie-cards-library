6a2bb1e38c08a25e72d19df3361440717e2da9b4
Rafael Azevedo <rafaelazevedo321@gmail.com>
2021-02-16 15:47:34 -0300

    fix lint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import MovieList from './components/MovieList';
+import Header from './components/Header.jsx';
+import MovieList from './components/MovieList.jsx';
 
 function App() {
+  const movies = [];
   return (
     <div className="App">
       <Header />
-      <MovieList movies={ [] }/>
+      <MovieList movies={ movies } />
     </div>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,20 +1,20 @@
 // // implement MovieCard component here
 import React from 'react';
-import Rating from './Rating';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
-   render() {
-     const movieInfo = this.props.movie;
-     return (
-       <div>
-         <img src={movieInfo.imagePath} alt="image renderizada"/>
-         <h4>{movieInfo.title}</h4>
-         <h5>{movieInfo.subtitle}</h5>
-         <p>{movieInfo.storyline}</p>
-         <Rating rating={ movieInfo.rating } />
-       </div>
-     );
+  render() {
+    const movieInfo = this.props.movie;
+    return (
+      <div>
+        <img src={movieInfo.imagePath} alt="image renderizada"/>
+        <h4>{movieInfo.title}</h4>
+        <h5>{movieInfo.subtitle}</h5>
+        <p>{movieInfo.storyline}</p>
+        <Rating rating={ movieInfo.rating } />
+      </div>
+    );
   }
 }
 
