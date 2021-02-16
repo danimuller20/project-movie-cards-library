ffa8912e4283ea1494d603dbf7489703443b4f7c
Raquel Picanço <picanco.raquel@gmail.com>
2021-02-16 09:58:08 -0400

    Create MovieList component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,13 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+
+class MovieCard extends Component {
+  render() {
+    return (
+      <section>
+
+      </section>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,41 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+
+    return (
+      <main>
+        {movies.map(({ title }) => (
+          <MovieCard key={ title } />
+        ))}
+      </main>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  })),
+};
+
+MovieList.defaultProps = {
+  movies: [
+    {
+      title: 'Movie Title',
+      subtitle: 'Movie Title',
+      storyline: 'Movie Storyline',
+      rating: 'Movie rating',
+      imagePath: 'File path to Movie banner',
+    },
+  ],
+};
+
+export default MovieList;
