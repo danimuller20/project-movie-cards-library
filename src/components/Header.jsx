c20516529b2f92ee38a06e5888c56a0725de0589
palenske <viniciuspalenske@gmail.com>
2021-02-17 16:41:49 -0300

    entrega do componente Header. Requisito 1 e 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,13 @@
-// Iniciando o projeto &#128640
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="movie-card-header">
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
