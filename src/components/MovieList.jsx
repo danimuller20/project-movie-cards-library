8382bf02877f21ff099f8f9f816716516f7662a5
Pamela Anderson Sales <pamelasalesr@gmail.com>
2021-02-18 10:47:03 -0300

    Add Header Component

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
-// implement MovieList component here
+import React from 'react';
+import '../App.css';
+import PropTypes from 'prop-types';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div className="">
+        <h1>test</h1>
+      </div>
+    );
+  }
+}
+
+export default MovieList;
