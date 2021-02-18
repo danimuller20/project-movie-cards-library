c1e2641cf8fe68ec470ed685bebb750049e0f717
Mathaus <mathausannibal@gmail.com>
2021-02-18 19:49:01 -0300

    correções

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
-
+import PropTypes from 'prop-types'
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
       <section>
+        {movies}
       </section>
     );
   }
 }
+
+MovieList.PropTypes = {
+  movies: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string, PropTypes.number))
+};
+
 
 
 export default MovieList;
