da0e76b4e337ac0584211d296752864fbb4988f9
Mathaus <mathausannibal@gmail.com>
2021-02-18 19:17:27 -0300

    correções

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,34 +1,15 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import movies from './data';
+
 
 class MovieList extends React.Component {
   render() {
-    const { title, subtitle, storyline, rating, imagePath } = this.props.movies;
+    const { movies } = this.props;
     return (
-      <div>
-        movies = [
-        {
-          { title },
-          { subtitle },
-          { storyline },
-          { rating },
-          { imagePath },
-        },
-        ];
-      </div>
+      <section>
+      </section>
     );
   }
 }
 
-movies.PropTypes = {
-  movies: PropTypes.shape({
-    title: PropTypes.string,
-    subtitle: PropTypes.string,
-    storyline: PropTypes.string,
-    rating: PropTypes.number,
-    imagePath: PropTypes.string,
-  }),
-};
 
 export default MovieList;
