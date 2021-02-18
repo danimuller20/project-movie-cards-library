25c4f2b5d7c5b58677433bc28a6190de7b23f4e0
Samuel <samuelvanguarda@gmail.com>
2021-02-18 12:18:23 -0300

    Requisito 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,13 @@
 // Iniciando o projeto 🚀
+
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header />
+    );
+  }
+}
+
+export default Header;
