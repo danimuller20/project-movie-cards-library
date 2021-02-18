5bb959b156977ac199dd54c18b45db22255407d7
Ana Luiza Machado Salgado <analuizamsalgado@gmail.com>
2021-02-18 00:28:27 -0300

    requirement 3 to 10

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 
 class Header extends React.Component {
   render() {
     return (
       <header>
-        <h1 className="title">Movie Cards Library</h1>
+        <h1 className='title'>Movie Cards Library</h1>
       </header>
     );
   }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,18 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
+    return (
+      <div>
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        <img alt={ title } src={ imagePath } />
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
