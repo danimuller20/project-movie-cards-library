9e6b1ec51b2169277fa11cc37c2ffec8a82c4cef
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-02-18 16:42:01 -0300

    all tasks

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,25 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props; // Isto esta certo?
+    console.log(movies);
+    return (
+      <section>
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      </section>
+    );
+  }
+}
+
+MovieList.proptTypes = {
+  movies: propTypes.arrayOf(
+    propTypes.shape({
+      title: propTypes.string,
+    }).isRequired,
+  ).isRequired,
+};
+
+export default MovieList;
