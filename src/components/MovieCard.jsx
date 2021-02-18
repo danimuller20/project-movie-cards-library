7ddbabf513095398835ad69ee3cccecfadb2b5f0
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2021-02-18 09:27:50 -0300

    segundo requisito

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -27,4 +27,12 @@ MovieCard.propTypes = {
   imagePath: PropTypes.string,
  };
 
+ MovieCard.defaultProps = {
+  title: 'teste',
+  subtitle: 'teste',
+  storyline: 'teste',
+  rating: 0,
+  imagePath: 'teste',
+ }
+
 export default MovieCard;
