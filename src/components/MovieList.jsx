a8afea59562ee76242eb52bfe4aa995d3e368fb6
Mathaus <mathausannibal@gmail.com>
2021-02-18 11:47:21 -0300

    adiciona item 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,19 @@
-// implement MovieList component here
+import React from 'react'
+
+class MovieList extends React.Component {
+  render() {
+    return(
+      <div>
+        movies = [
+          {
+            this.props.movies.title,
+            this.props.movies.subtitle,
+            this.props.movies.storyline
+          },
+        ];
+      </div> 
+    )
+  }
+}
+
+export default MovieList
