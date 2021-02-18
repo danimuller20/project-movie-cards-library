f5445893d7c09c36683e3de48dd6df1567ccd1d7
ViniciusChagas <viniciushrcs@gmail.com>
2021-02-18 20:07:38 -0300

    esqueleto pronto

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,23 @@
-// implement MovieCard component here
+import React from 'react';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
+    return (
+      <section>
+        <div>
+          <img src={ imagePath } alt={ title } />
+          <h4>{ title }</h4>
+          <h5>{ subtitle }</h5>
+          <p>{ storyline }</p>
+        </div>
+        <div>
+          <Rating rating={ rating } />
+        </div>
+      </section>
+    );
+  }
+}
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        { movies.map((currentMovie, index) => <MovieCard movie={ currentMovie } key={ index } />) }
+      </div>
+    )}
+}
+
+export default MovieList;
