cabb3c5417ea1fb09a0c8e81eb51fb120604003e
diego-vini <analistamm@gmail.com>
2021-02-18 11:52:46 -0300

    Ajustando espaços do movie card_3

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie } = this.props;  
-    const { title, subtitle, storyLine, rating, imagePath } = movie;
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <div>
         <p> 
