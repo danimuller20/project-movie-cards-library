d9fedfe0cdb1fe16b97eb33fb1208eb3ce4be507
Pamela Anderson Sales <pamelasalesr@gmail.com>
2021-02-18 15:02:45 -0300

    Add Rating component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../App.css';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -13,7 +14,9 @@ class MovieCard extends React.Component {
           <h4 className="movie-card-title">{title}</h4>
           <h5 className="movie-card-subtitle">{subtitle}</h5>
           <p className="movie-card-storyline">{storyline}</p>
-          <p>{rating}</p>
+          <div className="movie-card-rating">
+            <Rating rating={ rating } />
+          </div>
         </div>
       </section>
     );
