21fdbfbf727c98be9907b51919b8a0cc3b9646d5
Pamela Anderson Sales <pamelasalesr@gmail.com>
2021-02-18 11:23:23 -0300

    Fix linter issues on Header Component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
     <div className="App">
       {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
       <Header />
-      <MovieList />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,16 +1,1 @@
-import React from 'react';
-import '../App.css';
-import PropTypes from 'prop-types';
-
-class MovieList extends React.Component {
-  render() {
-    const { movies } = this.props;
-    return (
-      <div className="">
-        <h1>test</h1>
-      </div>
-    );
-  }
-}
-
-export default MovieList;
+// implement MovieList component here
