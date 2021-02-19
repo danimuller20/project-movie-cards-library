14b2f931a0bdacf84e01d9eaa40172ce56d88727
Mathaus <mathausannibal@gmail.com>
2021-02-18 22:52:27 -0300

    adiciona itens 8,9,10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,8 +6,8 @@ class MovieCard extends React.Component {
     const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
     return (
       <div>
-        <p>{ title }</p>
-        <p>{ subtitle }</p>
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
         <img src={ imagePath } alt="filme" />
         <p>{ rating }</p>
