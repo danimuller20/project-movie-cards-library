64a86790cb33551b6405c5e59cd29082c9f39695
cleber-teixeira <cleberlt@gmail.com>
2021-02-21 18:21:53 -0300

    Requisito 08 - Renderizar o título do filme dentro de uma tag "<h4 />". :white_check_mark:

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { /* title, subtitle, storyline,  */imagePath/* , rating */ } = movie;
+    const { title, /* subtitle, storyline,  */imagePath/* , rating */ } = movie;
     return (
       <div>
-        <img src={ imagePath } />
+        <img src={ imagePath } alt={ title } />
+        <h4>{ title }</h4>
         <p><Rating /></p>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
