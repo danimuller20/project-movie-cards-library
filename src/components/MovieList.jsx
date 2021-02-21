1aa0f7478ee94d065114ae9e61f4e446bc3dc897
jiarguello <jhonatan.arguello@gmail.com>
2021-02-21 13:33:11 -0300

    Mais uma tentativa de resolver issues.

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,9 +3,20 @@ import MovieCard from './MovieCard';
 
 class MovieList extends Component {
   render() {
+    const { movie } = this.props;
     return (
       <div>
-        <MovieCard movie={ this.props.movie } />
+        <MovieCard movie={ movie[0] } key="1" />
+        <MovieCard movie={ movie[1] } key="2" />
+        <MovieCard movie={ movie[2] } key="3" />
+        <MovieCard movie={ movie[3] } key="4" />
+        <MovieCard movie={ movie[4] } key="5" />
+
+      {/* <MovieList movie={ movies[0] } key="1" />
+      <MovieList movie={ movies[1] } key="2" />
+      <MovieList movie={ movies[2] } key="3" />
+      <MovieList movie={ movies[3] } key="4" />
+      <MovieList movie={ movies[4] } key="5" /> */}
       </div>
     );
   }
