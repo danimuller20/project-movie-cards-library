37da113467abcb30f7942900ed249125ce7fd0ed
Hudson Carlos <hc88806@gmail.com>
2021-02-21 00:39:52 -0400

    renderizando rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,22 @@
 // implement Rating component here
+import React from 'react';
+import { number } from 'prop-types';
+
+class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <h3>{ `Rating ${rating}` }</h3>
+    );
+  }
+}
+
+Rating.propTypes = {
+  rating: number,
+};
+
+Rating.defaultProps = {
+  rating: '?',
+};
+
+export default Rating;
