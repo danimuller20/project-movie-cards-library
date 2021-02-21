c4ae2cae32e562d144e147df30e0ff980bbeeceb
cleber-teixeira <cleberlt@gmail.com>
2021-02-21 02:09:38 -0300

    Requisito 12 - Renderizar a nota de um filme dentro de "Rating". :white_check_mark:

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 class Rating extends React.Component {
-  render() { }
+  render() {
+    const { rating } = this.props;
+    return (
+      <span className='rating'>{ rating }</span>
+    );
+  }
 }
 
 export default Rating;
