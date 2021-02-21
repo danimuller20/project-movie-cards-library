79ac3eb82badcc0a7428891d0023b41fc0d7bc1c
cleber-teixeira <cleberlt@gmail.com>
2021-02-21 02:49:57 -0300

    Requisito 12 - Refatoração "eslint". :white_check_mark:

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
     const { rating } = this.props;
     return (
-      <span className='rating'>{ rating }</span>
+      <span className="rating">{ rating }</span>
     );
   }
 }
+
+Rating.defaultProps = { rating: null };
+
+Rating.propTypes = { rating: PropTypes.number };
 
 export default Rating;
