cbd61d0cd6e1111d6abdbac527a8e5036cf34c68
cleber-teixeira <cleberlt@gmail.com>
2021-02-21 20:57:20 -0300

    Requisito 09 - Renderizar o subtítulo do filme dentro de uma tag "<h5>". :white_check_mark:

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,11 +5,12 @@ import Rating from './Rating';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, /* subtitle, storyline,  */imagePath/* , rating */ } = movie;
+    const { title, subtitle, /* storyline, */imagePath/* , rating */ } = movie;
     return (
       <div>
         <img src={ imagePath } alt={ title } />
         <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
         <p><Rating /></p>
       </div>
     );
@@ -19,6 +20,7 @@ class MovieCard extends React.Component {
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
     imagePath: PropTypes.string,
   }).isRequired,
 };
