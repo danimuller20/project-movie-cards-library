1afbdd2aa914780a39e920b6a88fe53d26fb8246
diego-vini <analistamm@gmail.com>
2021-02-21 22:09:29 -0300

    Iniciando componente app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import MovieList from './components/MovieList';
-import movies from './data'
+import movies from './data';
 
 function App() {
   return (
     <div className="App">
-      { <MovieList movies={ movies }/> }
+      <MovieList movies={ movies } />
     </div>
   );
 }
