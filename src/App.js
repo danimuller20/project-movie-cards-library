2ef935538c889f6ac5d768dedd1c229cb8d1d050
diego-vini <analistamm@gmail.com>
2021-02-21 22:20:45 -0300

    Reinderizando header dentro de app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import MovieList from './components/MovieList';
+import Header from './components/Header'
 import movies from './data';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <MovieList movies={ movies } />
     </div>
   );
