a9c8ccd7d1192aaa751ef99bed3ee79640577057
Juliana do Carmo <julianadocarmoti@gmail.com>
2021-02-22 08:44:55 -0300

    ajustes eslint

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,11 +5,11 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-          <header>
-              <h1>Project Movie Cards Library</h1>
-          </header>
-  );
-}
+      <header>
+        <h1>Project Movie Cards Library</h1>
+      </header>
+    );
+  }
 }
 
 export default Header;
