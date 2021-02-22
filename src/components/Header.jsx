025f8e2f9bb908943d53d54e112ebeaed75f16e5
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-02-22 19:20:47 -0300

    requisito 1 ok codeClimate 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
-class Header extends React.Component {
-  
+class Header extends React.Component {  
   render() {
-
     return (
       <header>
         <h1>Movie Cards Library</h1>
