2907da4f52391d79bef4be7f2a97930cfa071cb6
Juliana do Carmo <julianadocarmoti@gmail.com>
2021-02-22 17:17:07 -0300

    ajuste eslint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,16 +5,15 @@ import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie } = this.props;
     const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
 
     return (
       <section className="movie-card">
         <div>
-         <img src={ imagePath } alt={ title } className="movie-card-image" />
+          <img src={ imagePath } alt={ title } className="movie-card-image" />
         </div>
         <div className="movie-card-body">
-          <h4 className="movie-card-title">{ title }</h4> 
+          <h4 className="movie-card-title">{ title }</h4>
           <h5 className="movie-card-subtitle">{ subtitle }</h5>
           <p className="movie-card-storyline">{ storyline }</p>
         </div>
