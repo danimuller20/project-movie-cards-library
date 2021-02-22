916158e504fb129c33dc68342b0980112b4d2039
diego-vini <analistamm@gmail.com>
2021-02-21 21:39:39 -0300

    Ajusando espaços no rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -6,19 +6,18 @@ class Rating extends React.Component {
   render() {
     const { rating } = this.props;
     return (
-        <span>
-            { rating }
-        </span>
-    )
+      <span>
+        { rating }
+      </span>
+    );
   }
 }
 
 Rating.propTypes = {
-    rating: PropTypes.number,
-  };
-  
-  Rating.defaultProps = {
-    rating: null,
-  };
+  rating: PropTypes.number,
+};
+Rating.defaultProps = {
+  rating: null,
+};
 
 export default Rating;
