30826f628c235f3e88a71b712d5ae9e7459fb74e
alailsonko <aalailson3@gmail.com>
2021-02-21 22:59:48 -0300

    header

--- src/tests/header.test.js
+++ src/tests/header.test.js
@@ -11,7 +11,7 @@ describe('1 - Crie um componente `<Header />`', () => {
 describe('2 - Renderize um texto no `<Header />`', () => {
   let wrapper;
 
-  it('Renderize o texto "Movie Cards Library" dentro de `<Header />`', () => {
+  it('Renderize o texto "Movie Cards Library" dentro de `<Header />`', async () => {
     wrapper = shallow(<Header />);
     expect(wrapper.find('header h1').text()).toBe('Movie Cards Library');
   });
