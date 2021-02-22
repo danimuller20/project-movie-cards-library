b9a7ece986453593c5506f4661722733f5f68beb
Pablo Fernandes <pabloprates@gmail.com>
2021-02-22 15:42:31 -0300

    Requirements Three - Creating the MovieList Component

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,9 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render(moviesArray) {
+    const movies = moviesArray.map((movie) => `<li>${movie}<li>`);
+    return movies;
+  }
+}
+export default MovieList;
