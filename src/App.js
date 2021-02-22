eef8d62dd18289f99661f64b7142d65ca1013c88
diego-vini <analistamm@gmail.com>
2021-02-21 22:24:22 -0300

    Reinderizando header dentro de app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import MovieList from './components/MovieList';
-import Header from './components/Header'
+import Header from './components/Header';
 import movies from './data';
 
 function App() {
