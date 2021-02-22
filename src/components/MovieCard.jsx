0ca70dada49d55d3a177b361b39edd346dcf4d8c
alailsonko <aalailson3@gmail.com>
2021-02-21 23:43:47 -0300

    movie list

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,26 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
+
+const MovieCard = ({ movie }) => (
+
+  <div>
+    <img src={ `${movie.imagePath}` } alt="" />
+    <h4>{movie.title}</h4>
+    <h5>{movie.subtitle}</h5>
+    <p>{movie.storyline}</p>
+    <Rating rating={ movie.rating } />
+  </div>
+);
+
+MovieCard.propTypes = {
+  movie: PropTypes.objectOf({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+const MovieList = ({ movies }) => (
+  <header>
+    <h1>Movie Cards Library</h1>
+    {movies.map((movie) => <MovieCard key={ `${movie.title}` } movie={ movie } />)}
+  </header>
+);
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default MovieList;
