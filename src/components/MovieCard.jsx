d17167650cd03e0d271c14b936e9b9cc5760d41d
Juliana do Carmo <julianadocarmoti@gmail.com>
2021-02-22 15:23:35 -0300

    ajustes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,13 +4,14 @@ import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
     const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
 
     return (
       <section className="movie-card">
         <h1>Movie Cards</h1>
         <h2>{ title }</h2>
-        <img src= { imagePath } alt= { title } />
+        <img src={ imagePath } alt={ title } />
         <h3>{ subtitle }</h3>
         <h4>{ storyline }</h4>
         <h5>{ rating }</h5>
