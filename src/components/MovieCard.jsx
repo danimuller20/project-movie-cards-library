b7325c3fb2bf54829f74f32e2fec4dedb8cb7818
Juliana do Carmo <julianadocarmoti@gmail.com>
2021-02-22 14:33:49 -0300

    ajustes eslint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,12 +4,12 @@ import React from 'react';
 class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
-    
+
     return (
       <section className="movie-card">
         <h1>Movie Cards</h1>
         <h2>{ title }</h2>
-        <img src={ imagePath } alt={ title }/>
+        <img src= { imagePath } alt= { title } />
         <h3>{ subtitle }</h3>
         <h4>{ storyline }</h4>
         <h5>{ rating }</h5>
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,17 +2,15 @@
 import React from 'react';
 import MovieCard from './MovieCard';
 
-
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
       <section className="movie-list">
         <h1>Movie List</h1>
-        {movies.map((movie) => <MovieCard key={movie.title} movie={ movie } />)}
+        { movies.map((movie) => <MovieCard key={movie.title} movie={ movie } />) }
       </section>
     );
-  }
 }
 
 export default MovieList;
