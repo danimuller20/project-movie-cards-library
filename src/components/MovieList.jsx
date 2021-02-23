53d6ba209fa023f90eb2aed1ec5dae8dbbdabcc5
Bernardo Smith <bernardostech@gmail.com>
2021-02-23 11:15:08 -0500

    checando pendencias

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import MovieCard from './MovieCard'
-
 class MovieList extends React.Component {
-    render() {
-        
-        return (
-          this.props.map((movie, index) => <MovieCard key={index} movie = {movie}/>)
-           
-        
-        )
-    }
+  render() {
+    return (
+    this.props.map((movie, index) => <MovieCard key={ index } movie = { movie }/>)
+    );
+  }
 }
 
 export default MovieList;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 
-
 class Rating extends React.Component {
-    render() {
-        const {rating} = this.props;
-        return (
-        <div>
-            {rating}
-        </div>
-        );
-    } 
+  render() {
+    const { rating } = this.props;
+    return (
+    <div>
+        {rating}
+    </div>
+    );
+  }
 }
 
 export default Rating;
