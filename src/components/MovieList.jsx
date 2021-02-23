ee9950a9e196be50e06d20528477c51caca0ef25
Thamirys <thamirysgoncalves.prog@gmail.com>
2021-02-23 19:17:25 -0300

    Realizado requisito 03

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieList extends React.Component {
+  render() {
+    return (<section />);
+  }
+}
+
+MovieList.PropType = {
+  data: PropTypes.array,
+};
+
+export default MovieList;
