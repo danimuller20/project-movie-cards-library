59ffee07933e7815bd58d484f1524ccf721ebad6
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-02-23 03:36:14 -0300

    CodeClimate issues 4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,24 +5,27 @@ import PropTypes from 'prop-types'
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <div>
-        <h4>{ movie.title }</h4>
-        <h5>{ movie.subtitle }</h5>
-        <p>{ movie.storyline }</p>
-        <img src={ movie.imagePath } alt="" />
-        <Rating rating={ movie.rating } />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <img src={ imagePath } alt="" />
+        <Rating rating={ rating } />
       </div>
     );
   }
 }
 
 MovieCard.propType = {
-  title: PropTypes.string,
-  subtitle: PropTypes.string,
-  storyline: PropTypes.string,
-  imagePath: PropTypes.string,
-  rating: PropTypes.number,
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  })  
 }
 
 export default MovieCard;
