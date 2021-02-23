d06ad18e33726c8bb3543f015b44db58d8937e5a
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-02-23 10:09:40 -0300

    Criado o item header e inserido o texto Movie Cards Library, requisitos 1 e 2.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Header from './components/Header';
+import MovieList from './components/MovieList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
-      Hora de se matar de codaaaaar
+      <Header titleText="Movie Cards Library" />
+      <MovieList />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,9 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return <p>Movie Cards Library</p>;
+  }
+}
+
+export default MovieList;
