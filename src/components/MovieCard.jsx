2886209372d4b84b9a9bba1d89f62cdddf535fd5
Caio <caioqs.info@gmail.com>
2021-02-23 11:48:19 -0300

    Criado todos os componentes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,22 @@
 // implement MovieCard component here
+import React, { Component } from 'react';
+import Rating from './Rating';
+
+
+class MovieCard extends Component {
+  render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+    return (
+      <div>
+        <img src={ imagePath } alt={ title } />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        <Rating rating={rating}/>
+      </div>
+    );
+  }
+}
+
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
 // implement MovieList component here
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      movies.map((element) => <MovieCard key={element.title} movie={element} />)
+    );
+  }
+}
+
+
+export default MovieList;
