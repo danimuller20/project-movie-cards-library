1866b21b55cb831579db6b71fc282bc46bc4dfc5
Emanoel Leão <emanuelleaomarinho@gmail.com>
2021-02-23 12:26:46 -0300

    Renderizando MovieCard e MovieList

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
+import movies from './data';
 
 function App() {
+  console.log(movies);
   return (
     <div className="App">
       <Header />
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,18 @@
-// implement MovieCard component here
+import React from 'react';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <section>
+        <img src={ this.props.imagePath}></img>
+        <h4>{ this.props.title }</h4>
+        <h5>{ this.props.subtitle }</h5>
+        <p>{ this.props.storyline }</p>
+        <Rating />
+      </section>
+    );
+  }
+}
+
+export default MovieCard;
