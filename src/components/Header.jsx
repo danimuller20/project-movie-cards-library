ff8ecbc7b0522c7ce6d91027ca0e3da56a9c1f02
WagnerMor <wagnermor@outlook.com>
2021-02-23 17:41:54 -0300

    all project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,13 @@
-// implement Header component here
+import React from 'react';
+
+const { Component } = React;
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header className="movie-card-header">
+        <h1 className="page-title">Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,23 @@
-// implement Rating component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Rating extends Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <div className="movie-card-rating">
+        <div className="rating">
+          {rating}
+        </div>
+      </div>
+    );
+  }
+}
+
+Rating.defaultProps = {
+  rating: 0,
+};
+
+Rating.propTypes = {
+  rating: PropTypes.number,
+};
