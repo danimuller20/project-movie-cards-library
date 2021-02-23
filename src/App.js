fea0c5f0cf507b9dcb5555e8f0ba41a902602668
Perycles floriano <perycles_reis@hotamil.com>
2021-02-23 19:09:07 -0300

    requisitos 1 e 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Header from './components/Header';
 import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
     </div>
   );
 }
