7c42a9d885cd54ca054cfdb1115551853ec52154
Raphael Rêgo <raphael.mlrego@gmail.com>
2021-02-24 17:16:29 -0300

    Requisitos 1 e 2 - HEADER

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,12 @@
-// implement Header component here.
+import React from 'react';
+
+function header () {
+  return (
+    <header>
+      <h1>
+        Movie Cards Library
+      </h1>
+    </header>
+  )
+}
+export default header;
