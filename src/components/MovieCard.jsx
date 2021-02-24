4fd0ab6f9776a713cc0ea906893cd79b2f9a06e1
Matheus Monteiro <monteiro@pop-os.localdomain>
2021-02-24 17:05:11 -0300

    requisitos | MovieList.jsx + MovieCard.jsx + terceira correcao eslint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,7 +7,7 @@ class MovieCard extends React.Component {
     const { movie: { title, subtitle, storyline, imagePath } } = this.props;
     return (
       <div>
-        <img src={ imagePath } alt="image of the game" />
+        <img src={ imagePath } alt="game" />
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
@@ -21,7 +21,6 @@ MovieCard.propTypes = {
     title: PropTypes.string,
     subtitle: PropTypes.string,
     storyline: PropTypes.string,
-    rating: PropTypes.number,
     imagePath: PropTypes.string,
   }).isRequired,
 };
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,22 +1,22 @@
 // implement MovieList component here
 import React from 'react';
-import MovieCard from './MovieCard';
 import PropTypes from 'prop-types';
-
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
+    console.log(movies);
     return (
       <div>
-        { movies.map(currentValue => <MovieCard movie={ currentValue } key={ currentValue.title } />)}
+        { movies.map((currentValue) => (
+          <MovieCard movie={ currentValue } key={ currentValue.title } />
+        ))}
       </div>
     );
   }
 }
 
-MovieList.propTypes = {
-  movies: PropTypes.array.isRequired,
-}
+MovieList.propTypes = { movies: PropTypes.arrayOf(PropTypes.object).isRequired };
 
 export default MovieList;
