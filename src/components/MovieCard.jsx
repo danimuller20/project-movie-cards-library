57c6b0d23f6abfbf50912d3cee23a169c636e787
Henrique-Moura <henrique_hgm1996@hotmail.com>
2021-02-24 16:38:10 -0300

    Style da pag

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,12 +8,14 @@ class MovieCard extends React.Component {
     const { title, subtitle, storyline, imagePath, rating } = movie;
 
     return (
-      <div>
-        <img src={ imagePath } alt="Imagem capa do filme" />
-        <h4>{ title }</h4>
-        <h5>{ subtitle }</h5>
-        <p>{ storyline }</p>
-        <Rating rating={ rating } />
+      <div className="movie-card">
+        <img className="movie-card-image" src={ imagePath } alt="Imagem capa do filme" />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
+          <Rating rating={ rating } />
+        </div>
       </div>
     );
   }
