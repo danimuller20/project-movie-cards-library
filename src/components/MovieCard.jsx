323a41a129aae0d25d68b79b4efcf9e0f7f0df96
Matheus Monteiro <monteiro@pop-os.localdomain>
2021-02-24 01:10:16 -0300

    requisitos | MovieList.jsx + MovieCard.jsx

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,17 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <div>
+        <img src={this.props.movie.imagePath} />
+        <h4>{this.props.movie.title}</h4>
+        <h5>{this.props.movie.subtitle}</h5>
+        <p>{this.props.movie.storyline}</p>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.movies.map((currentValue) => {
+          return <MovieCard movie={currentValue} key={currentValue.title} />;
+        })}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
