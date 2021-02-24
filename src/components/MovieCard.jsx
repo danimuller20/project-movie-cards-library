fed5cef2e07e7b5bcfba987c51194a78034d0fb2
Bernardo Smith <bernardostech@gmail.com>
2021-02-23 21:13:11 -0500

    ajustando o codigo

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,25 +4,16 @@ import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, subtitle, storyline, imagePath, rating } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
       <section className="movie-card-body">
         <div className="movie-card">
           <img className="movie-card-image" src={ imagePath } alt={ `${title}` } />
         </div>
         <div>
-          <h4 className="movie-card-title">
-            Title:
-            {title}
-          </h4>
-          <h5 className="movie-card-subtitle">
-            Subtitle:
-            {subtitle}
-          </h5>
-          <p className="movie-card-storyline">
-            Sinopse:
-            {storyline}
-          </p>
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
         </div>
         <div className="movie-card-rating">
           <Rating rating={ rating } />
