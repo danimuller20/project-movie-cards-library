3dc91418f75e0dc041775d90f945912f22c7aa7a
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-02-24 13:52:52 -0300

    Commit com as a montagem do projeto faltando as validaçoes dos proptypes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,22 @@
-// implement MovieCard component here
+import React from 'react';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie: { title, subtitle, rating, storyline, imagePath } } = this.props;
+
+    return (
+      <div className="movie-card">
+        <h4 className="movie-card-title">{title}</h4>
+        <h5 className="movie-card-subtitle">{subtitle}</h5>
+        <p className="movie-card-storyline">{storyline}</p>
+        <img className="movie-card-image" src={ imagePath } alt={ `${title} poster` } />
+        <div className="movie-card-rating">
+          <Rating rating={ rating } />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    return <p>Movie Cards Library</p>;
+    const { movies } = this.props;
+
+    return (
+      <div className="movie-list">
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      </div>
+    );
   }
 }
 
