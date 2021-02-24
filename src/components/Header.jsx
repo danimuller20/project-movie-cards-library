35ea343b53cc6bebe908a40f1346ae1170b34719
Raphael Rêgo <raphael.mlrego@gmail.com>
2021-02-24 17:23:58 -0300

    correcao lint requisitos 1 e 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-function header () {
+function header() {
   return (
     <header>
       <h1>
         Movie Cards Library
       </h1>
     </header>
-  )
+  );
 }
 export default header;
