b11958e63edd8304ae3a105bdc97909cdc885ad5
Kevin Fraga <kevinciolac@gmail.com>
2021-02-24 02:06:37 -0300

    :three::four::five: MovieList Component - fix :one: reworks for linter

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,15 +1,23 @@
 // implement MovieList component here
 import React, { Component } from 'react';
 import MovieCard from './MovieCard';
+import PropTypes from 'prop-types';
 
 class MovieList extends Component {
   render() {
+    const { movies } = this.props;
     return (
       <div>
-        {this.props.movies.map((movie) => <MovieCard movie={movie} key={movie.title} />)}
+        {movies.map((movie) => <MovieCard movie={ movie } key={ movie.title } />)}
       </div>
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+  }))
+};
 
 export default MovieList;
