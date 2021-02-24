1f85c568594066c9971165edb94256c900015a7e
Caio <caioqs.info@gmail.com>
2021-02-24 08:46:28 -0300

    Feito projeto v2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,13 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
-    render() {
-        return (
-            <header className='.movie-card-header'>
-                <h1>Movie Cards Library</h1>
-            </header>
-        );
-    }
+  render() {
+    return (
+      <header className="movie-card-header">
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
 }
 
 export default Header;
