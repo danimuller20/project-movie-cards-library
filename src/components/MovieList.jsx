eaf2615fc23dc6bb280f0a0ea93246d1fbf6ed0a
Kevin Fraga <kevinciolac@gmail.com>
2021-02-24 01:56:15 -0300

    :three::four::five: MovieList Component

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
 // implement MovieList component here
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.movies.map((movie) => <MovieCard movie={movie} key={movie.title} />)}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
