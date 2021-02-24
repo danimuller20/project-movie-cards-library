126934ecd9522a9ac3726cbad0e782f3b2deef9f
Matheus Monteiro <monteiro@pop-os.localdomain>
2021-02-24 15:40:47 -0300

    requisitos | MovieList.jsx + MovieCard.jsx + segimda correcao eslint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,13 @@
 // implement MovieCard component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
     const { movie: { title, subtitle, storyline, imagePath } } = this.props;
     return (
       <div>
-        <img src={ imagePath } alt="image" />
+        <img src={ imagePath } alt="image of the game" />
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
@@ -14,5 +15,15 @@ class MovieCard extends React.Component {
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,18 +1,22 @@
 // implement MovieList component here
 import React from 'react';
 import MovieCard from './MovieCard';
+import PropTypes from 'prop-types';
+
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
       <div>
-        { movies.map(currentValue => {
-          return <MovieCard movie={ currentValue } key={ currentValue.title } />;
-        })}
+        { movies.map(currentValue => <MovieCard movie={ currentValue } key={ currentValue.title } />)}
       </div>
     );
   }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.array.isRequired,
 }
 
 export default MovieList;
