22c3c0dc472c2164c9597f9b08a78144a85837d5
Caio <caioqs.info@gmail.com>
2021-02-24 08:39:47 -0300

    Feito projeto

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,19 +1,19 @@
 // implement MovieList component here
 import React, { Component } from 'react';
-import MovieCard from './MovieCard';
 import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends Component {
   render() {
     const { movies } = this.props;
     return (
-      movies.map((element) => <MovieCard key={element.title} movie={element} />)
+      movies.map((element) => <MovieCard key={ element.title } movie={ element } />)
     );
   }
 }
 
 MovieList.propTypes = {
-  movie: PropTypes.object,
-}
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -4,16 +4,15 @@ import PropTypes from 'prop-types';
 
 class Rating extends Component {
   render() {
-      const { rating } = this.props;
-      return (
-        <div className='rating'>{rating}</div>
-      );
+    const { rating } = this.props;
+    return (
+      <div className="rating">{rating}</div>
+    );
   }
 }
 
-Rating.protoType = {
-  rating: PropTypes.number,
-}
-
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
 
 export default Rating;
