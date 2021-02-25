97f6bb8d0cc12e08ec2ae6b32096f7f643c323ce
jorgeramires <jorgerl123@gmail.com>
2021-02-25 14:35:31 -0300

    resolvendo terceiro exercicio

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from 'react';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
-    render (){
-        const {movies} = this. props;
-        return (
+  render() {
+    const { movies } = this.props;
+    return (
             movies.map((movie) => <MovieCard movie={ movie } key={ movie.title } />)
-        );
-    }
+    );
+  }
 }
 
 export default MovieList
