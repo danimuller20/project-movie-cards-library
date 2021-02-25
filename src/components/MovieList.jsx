a2ed80ddf76bee686f27707a3cb14e36cb6870e2
jorgeramires <jorgerl123@gmail.com>
2021-02-25 15:04:02 -0300

    resolvendo terceiro exercicio

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
+
 class MovieList extends React.Component {
   render() {
-    const { movies } = this.props;
-      return (
+  const { movies } = this.props;
+    return (
             movies.map((movie) => <MovieCard movie={ movie } key={ movie.title } />)
     );
-  }
 }
-  MovieList.propTypes = {
+}
+    MovieList.propTypes = {
 movies: PropTypes.arrayOf(PropTypes.object).isRequired,
   };
 export default MovieList;
