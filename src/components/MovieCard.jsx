047e35e09e53662fd09ec5c1f26ea5057a05ddb5
Micaela Alves <micaela.alves93@gmail.com>
2021-02-25 02:56:59 -0300

    styles

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,13 +6,18 @@ import Rating from './Rating';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
+    const { imagePath, title, subtitle, storyline, rating } = movie;
     return (
-      <div>
-        <img src={ movie.imagePath } alt="" />
-        <h4>{movie.title}</h4>
-        <h5>{movie.subtitle}</h5>
-        <p>{movie.storyline}</p>
-        <Rating rating={ movie.rating } />
+      <div className="movie-card">
+        <img className="movie-card-image" src={ imagePath } alt="" />
+        <body className="movie-card-body">
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
+        </body>
+        <div className="movie-card-rating">
+          <Rating rating={ rating } />
+        </div>
       </div>
     );
   }
