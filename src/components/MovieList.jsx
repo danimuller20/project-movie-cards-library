32b2d488be960b1552c4b75c19b7371575993be9
Marina Alane <marina.alane@outlook.com>
2021-02-25 09:47:05 -0300

    renderiza movie-list

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,8 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (<div className="movie-list" />);
+  }
+}
+export default MovieList;
