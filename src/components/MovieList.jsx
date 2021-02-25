c73f26b98c0fd07e5bb10b8d1399eb6c4e6ceb9e
jorgeramires <jorgerl123@gmail.com>
2021-02-25 15:01:18 -0300

    resolvendo terceiro exercicio

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
-
-
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
