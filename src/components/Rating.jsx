f64d2d4314dc7b2dcb45e8934f4a836acaa84aa3
Igor Machado <rf59imft@gmail.com>
2021-02-25 18:29:58 -0300

    desafio 11 13 14 12

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,14 @@
-// implement Rating component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Rating = ({ rating }) => (
+  <div>
+    <h1 className="rating">{rating}</h1>
+  </div>
+);
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
+
+export default Rating;
