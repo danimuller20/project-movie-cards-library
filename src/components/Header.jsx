a0684cac6d8cbcc2d7b1af90dea4a9f271049073
Perycles floriano <perycles_reis@hotamil.com>
2021-02-25 17:25:26 -0300

    refatorando

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ class Header extends Component {
   render() {
     return (
       <header className="header">
-        <h4>Movie Cards Library</h4>
+        <h1>Movie Cards Library</h1>
       </header>
     );
   }
