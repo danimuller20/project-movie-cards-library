cff1a76ffb29a0b63f5182ab9ff98b4971bf5756
Marina Alane <marina.alane@outlook.com>
2021-02-25 18:53:13 -0300

    tentando mais uma vez ajustar os requisitos quebrados

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    return (<div className="movie-list" />);
+    const { movies } = this.props;
+    return (
+      <div>
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      </div>
+    );
   }
 }
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  })),
+};
+
+MovieList.defaultProps = {
+  movies: [
+    {
+      title: 'Movie Title',
+      subtitle: 'Movie Title',
+      storyline: 'Movie Storyline',
+      rating: 'Movie rating',
+      imagePath: 'File path to Movie banner',
+    },
+  ],
+};
+
 export default MovieList;
