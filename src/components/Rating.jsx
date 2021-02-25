f88ea27b89e86068313805a2546358d16c12dcda
Kevin Fraga <kevinciolac@gmail.com>
2021-02-25 15:36:33 -0300

    :one::one:, :one::two: Rating component - fix :one: minor fixes for linter

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -4,16 +4,17 @@ import PropTypes from 'prop-types';
 
 class Rating extends Component {
   render() {
-    return <p className='rating'>{ this.props.rating }</p>;
+    const { rating } = this.props;
+    return <p className="rating">{rating}</p>;
   }
 }
 
 Rating.propTypes = {
   rating: PropTypes.string,
-}
+};
 
 Rating.defaultProps = {
   rating: '3',
-}
+};
 
 export default Rating;
