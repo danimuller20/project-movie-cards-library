e59803635830e055584615b118102d02389a6a5b
Kevin Fraga <kevinciolac@gmail.com>
2021-02-25 15:27:31 -0300

    :one::one:, :one::two: Rating component

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,19 @@
 // implement Rating component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends Component {
+  render() {
+    return <p className='rating'>{ this.props.rating }</p>;
+  }
+}
+
+Rating.propTypes = {
+  rating: PropTypes.string,
+}
+
+Rating.defaultProps = {
+  rating: '3',
+}
+
+export default Rating;
