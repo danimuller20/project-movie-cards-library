3b55719b55edf50f15af115ed3c16da6de1ca5e0
Micaela Alves <micaela.alves93@gmail.com>
2021-02-25 00:32:03 -0300

    elemento Rating - propTypes

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,15 +1,20 @@
 // implement Rating component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
-    render() {
-      const { rating } = this.props;
-      return (
-        <div>
-          <p className="rating">{rating}</p>
-        </div>
-      );
-    }
+  render() {
+    const { rating } = this.props;
+    return (
+      <div>
+        <p className="rating">{rating}</p>
+      </div>
+    );
   }
-  
-  export default Rating;
+}
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
+
+export default Rating;
