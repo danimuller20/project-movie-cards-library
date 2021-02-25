4077ea753dd7fad270e11a45d470c5a6997a0a9b
Marina Alane <marina.alane@outlook.com>
2021-02-25 13:50:44 -0300

    ajustando requisitos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import MovieCard from './components/MovieCard';
 import MovieList from './components/MovieList';
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
     <div className="App">
       <Header />
       <MovieList />
-      <MovieCard />
     </div>
   );
 }
