b5c093c3d1a877bf1472c2c8a127e2036619960d
Micaela Alves <micaela.alves93@gmail.com>
2021-02-25 00:12:42 -0300

    elemento Rating - requisitos 11, 12, 13 e 14

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,15 +1,15 @@
 // implement Rating component here
 import React from 'react';
-import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
     render() {
+      const { rating } = this.props;
       return (
         <div>
-
+          <p className="rating">{rating}</p>
         </div>
       );
     }
-}
-
-export default Rating.jsx;
+  }
+  
+  export default Rating;
