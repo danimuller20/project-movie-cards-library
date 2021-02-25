d1b8260a8fafe93a90340307f61bcef6c3e66870
LeonarDev <leonardo.majevski@gmail.com>
2021-02-25 19:47:12 -0300

    test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Header from './components/Header';
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header text={'xablau'}/>
+      <br />
       Hora de codar! 🚀✍️👨‍💻👩‍💻💪
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <div>xablau</div>
+      <header>{this.props.text}</header>
     )
   }
 }
