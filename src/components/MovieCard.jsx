8f70ebcd254a5a11b30a345afb1e13296ce6bf88
jorgeramires <jorgerl123@gmail.com>
2021-02-25 15:37:05 -0300

    tentando resolver exercicio

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
     render() {
-    
+        const { movie } = this.props;
+        return (
+            <div>
+                <h4>{movie.title}</h4>
+                <h5>{movie.subtitle}</h5>
+                <p>{movie.soryline}</p>
+                <img src={movie.imagePath}/>
+                <Rating rating = {movie.ratings}/>
+            </div>
+        )
     }
 }
+MovieCard.propTypes = {
+    movie : PropTypes.exact({
+        title: PropTypes.string,
+        subtitle: PropTypes.string,
+        storyLine: PropTypes.string,
+        imagePath: PropTypes.string,
+        rating: PropTypes.number,
+    }).isRequired,
+    };
 
 export default MovieCard;
