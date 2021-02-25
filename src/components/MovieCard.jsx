279e1dd17c4695db42b8d9ca4b39d47ed9f3b151
jorgeramires <jorgerl123@gmail.com>
2021-02-25 15:46:36 -0300

    tentando resolver exercicio

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-      return (
-            <div>
-                <h4>{ movie.title }</h4>
-                <h5>{ movie.subtitle }</h5>
-                <p>{ movie.soryline }</p>
-                <img src={ movie.imagePath }/>
-                <Rating rating = { movie.ratings }/>
+    return (
+    <div>
+      <h4>{ movie.title }</h4>
+      <h5>{ movie.subtitle }</h5>
+      <p>{ movie.storyline }</p>
+      <img src={ movie.imagePath }/>
+      <Rating rating = { movie.ratings }/>
       </div>
     );
   }
