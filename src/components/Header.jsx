d2ea229505a0efdcac00cef5c653aa53e0cc9b4d
Igor Machado <rf59imft@gmail.com>
2021-02-25 17:39:20 -0300

    desafio 01 e 02

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from  'react';
+import React,{ Component } from  'react';
 
 class Header extends Component {
   render() {
@@ -6,7 +6,7 @@ class Header extends Component {
       <header className="header">
         <h1>Movie Cards Library</h1>
       </header>
-    )
+    );
   }
 }
 
