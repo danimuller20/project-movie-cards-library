441170b4ac5dad489841d07757445ce9d83aa027
Marina Alane <marina.alane@outlook.com>
2021-02-25 13:25:53 -0300

    usando propTypes

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import propTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
-class MovieList extends React.Component {
+class MovieList extends Component {
   render() {
-    this.props.movie.map((movie, index) => <MovieCard key={ index } movie={ movie } />);
+    const { movies } = this.props;
     return (
       <div>
-        <ul>
-          <li>{ movie }</li>
-        </ul>
+        {movies.map((movie) => <MovieCard movie={ movie } key={ movie.title } />)}
       </div>
     );
   }
 }
 
+MovieList.propTypes = {
+  movies: propTypes.arrayOf(PropTypes.shape({
+    title: propTypes.string,
+    subtitle: propTypes.string,
+    storyline: propTypes.string,
+  })),
+};
+
+MovieList.defaultProps = {
+  movies: [
+    {
+      title: 'TitleMovie',
+      subtitle: 'TitleMovie',
+      storyline: 'MovieStoryLine',
+    },
+  ],
+};
 export default MovieList;
+
+// Para os requisitos onde foi necessário o proptype e o defaultProps, consultei o repositório da colega 
+// Raquel Picanço e consultei a estrutura utilizada.
