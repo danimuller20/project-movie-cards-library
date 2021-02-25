0d9f464e8e6005286e98bcaf3357c472953edc8d
jorgeramires <jorgerl123@gmail.com>
2021-02-25 15:54:43 -0300

    tentando resolver exercicio

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,13 +5,13 @@ class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
     return (
-    <div>
-       <h4>{ movie.title }</h4>
-      <h5>{ movie.subtitle }</h5>
-      <p>{ movie.storyline }</p>
-      <img src={ movie.imagePath }/>
-      <Rating rating = { movie.ratings }/>
-      </div>
+      <div>
+        <h4>{ movie.title }</h4>
+        <h5>{ movie.subtitle }</h5>
+        <p>{ movie.storyline }</p>
+        <img src={ movie.imagePath }/>
+        <Rating rating = { movie.ratings }/>
+    </div>
     );
   }
 }
