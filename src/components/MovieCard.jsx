232326ba3773b7f6cf629942ae93bd7f1a067d66
Emanoel Leão <emanuelleaomarinho@gmail.com>
2021-02-24 21:14:04 -0300

    Alinhando MovieCard.js ao lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,11 +6,11 @@ class MovieCard extends React.Component {
     const { imagePath, title, subtitle, storyline, rating } = this.props.movie;
     return (
       <section>
-        <img src={ imagePath} />
+        <img src={ imagePath } />
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
-        <Rating rating={ rating }/>
+        <Rating rating={ rating } />
       </section>
     );
   }
