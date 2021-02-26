afceba870ddbd545d8b06eab4e75c64dd38f5a92
Marina Alane <marina.alane@outlook.com>
2021-02-26 09:28:42 -0300

    ajusta eslint]

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -15,6 +15,7 @@ class MovieCard extends Component {
         />
         <h4 className="movie-card-title">{ title }</h4>
         <h5 className="movie-card-subtitle">{ subtitle }</h5>
+        <Rating rating={ rating } />
         <p className="movie-card-storyline">{ storyline }</p>
       </div>
     );
