b7f0ea6894d9b27b1d4627ebbd8370dfbfd46a67
Marina Alane <marina.alane@outlook.com>
2021-02-25 21:22:15 -0300

    adiciona rating

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
+import Rating from './components/Rating';
 
 class App extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class App extends React.Component {
       <div className="App">
         <Header />
         <MovieList />
+        <Rating />
       </div>
     );
   }
