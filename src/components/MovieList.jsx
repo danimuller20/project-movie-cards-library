eb770cc4925059b5b4ab8d73387d0011b86a698f
ederson rodrigo <edersonrodrigo31@gmail.com>
2021-02-26 01:26:32 -0300

    terceiro requisito

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,13 +3,15 @@ import PropTypes from 'prop-types';
 
 class MovieList extends Component {
   render() {
-    const { movies } = props;
-    return <div>{movies}</div>;
+    const { movies } = this.propsprops;
+    return (
+      <div>{movies}</div>
+    );
   }
 }
 
-MovieList.prototype = {
-  movies: PropTypes.array,
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default MovieList;
