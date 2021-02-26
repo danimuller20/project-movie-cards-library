9e745d1fa556cb00ebb9c32dcb1f3a0baa84cf9a
MichaelMascarenhas <michaelmascarenhastrybe@gmail.com>
2021-02-26 18:26:06 -0300

    class Header implementada e rodando perfeitamente

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,12 @@
 // iniciando o projeto fugueti
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return <header>
+            <h1>Cabeçalho do projeto</h1>
+        </header>
+    }
+}
+
+export default Header;
