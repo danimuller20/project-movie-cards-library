ce2b7fbdcc1ab7bf373f0cc4c9df8a21e9d35ec4
Felipe Pimentel <lipe.pimentel89@gmail.com>
2021-02-25 23:44:29 -0300

    correcao classes css

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,11 +9,13 @@ class MovieCard extends React.Component {
     const { movie } = this.props;
     const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
-      <div className="movie-card movie-card-body">
+      <div className="movie-card">
         <img className="movie-card-image" src={ imagePath } alt="" />
-        <h4 className="movie-card-title">{title}</h4>
-        <h5 className="movie-card-subtitle">{subtitle}</h5>
-        <p className="movie-card-storyline">{storyline}</p>
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+        </div>
         <Rating rating={ rating } />
       </div>
     );
