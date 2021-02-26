34ff29b19b9a094cbc533cc4baa4b00ffcead80a
Luiz Vicentin <luizpavanello@gmail.com>
2021-02-26 20:38:13 -0300

    Fixed Eslint MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -14,6 +14,6 @@ class MovieList extends React.Component {
   }
 }
 
-MovieList.PropTypes = { movies: PropTypes.arrayOf.isRequired };
+MovieList.propTypes = { movies: PropTypes.arrayOf.isRequired };
 
 export default MovieList;
