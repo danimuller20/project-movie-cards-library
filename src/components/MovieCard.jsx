2fd4e8a52728ae0cff0be657be26aedb784031bd
Felipe Pimentel <lipe.pimentel89@gmail.com>
2021-02-25 21:41:15 -0300

    commitao bomba MovieCard + MovieList + classes css

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,19 @@
 // implement MovieCard component here
+
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, imagePath } = this.props.movie
+    return (
+      <div className='movie-card movie-card-body'>
+        <img className='movie-card-image' src={imagePath} alt=''></img>
+        <h4 className='movie-card-title'>{title}</h4>
+        <h5 className='movie-card-subtitle'>{subtitle}</h5>
+        <p className='movie-card-storyline'>{storyline}</p>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
