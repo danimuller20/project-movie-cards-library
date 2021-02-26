80ebf5ec05ac0075616ffef3950d9c61bcb6e9c1
Luiz Vicentin <luizpavanello@gmail.com>
2021-02-26 20:14:28 -0300

    MovieList is created and render

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,18 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+import PropTypes from 'prop-types';
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        {movies.map((movie) => <MovieCard movie={ movie } key={ movie.title } />)}
+      </div>
+    );
+  }
+}
+
+MovieList.PropTypes = { movies: PropTypes.arrayOf.isRequired };
+
+export default MovieList;
