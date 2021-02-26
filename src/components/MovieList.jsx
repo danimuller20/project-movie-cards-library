26617a0f98fd90c9e3a4384ef5047e0f85cc0c91
MarceloPelegrino <pelegrinocorrea@gmail>
2021-02-25 21:35:17 -0300

    Requisitos 1, 2 e 3 Dia 25-02-2021 21:35

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
 // implement MovieList component here
+import React from 'react';
+import movies from '../data';
+
+class MovieList extends React.Component {
+    render() {
+        const moviesList = movies.map((movie, index) => {});
+        return (
+            movies.map(movie => (<div>
+                <MovieList moviesList={movie} />
+            </div>
+        )))
+    }
+}
+
+export default MovieList;
