d73ae23169bb69b6ee2f272177c6b93bcf354e02
ederson rodrigo <edersonrodrigo31@gmail.com>
2021-02-26 02:23:43 -0300

    Moviecard component

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    const {
+      title,
+      subtitle,
+      storyline,
+      imagePath,
+    } = movie;
+
+    return (
+      <div>
+        <img src={ imagePath } alt={ title } />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends Component {
   render() {
     const { movies } = this.props;
     return (
       <div>
-        {movies}
+        {movies.map(
+          (movie) => <MovieCard key={ movie.title } movie={ movie } />,
+        )}
       </div>
     );
   }
