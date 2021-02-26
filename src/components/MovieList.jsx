979a8a71a8e93917886ca2ff5eb7049a46ea45c2
Miguel Dantas <miiguelalb@gmail.com>
2021-02-26 11:45:22 -0300

    Corrigido erros no ESLint

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,9 @@
-// implement MovieList component here
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
-  render () {
+  render() {
     const { movies } = this.props;
     return (
       <div className="movie-list">
