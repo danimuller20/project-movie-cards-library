9df4b0e3995bb9ddb8fcaa8c29b7d9b8f979e8be
Miguel Dantas <miiguelalb@gmail.com>
2021-02-26 11:36:44 -0300

    Adcionado recursos no componente MovieList

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 class Header extends React.Component {
-  render () {
-    return ( 
+  render (){
+    return (
       <div>
         <header className="movie-card-header">
           <h1 className="page-title">Movie Cards Library</h1>
