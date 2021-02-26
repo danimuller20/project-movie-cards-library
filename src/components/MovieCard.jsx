04b1751b2f5def96d87fdf231cb15862fd014fe2
Felipe Pimentel <lipe.pimentel89@gmail.com>
2021-02-25 22:37:37 -0300

    lint 3

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,10 +5,11 @@ import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, subtitle, storyline, imagePath } = this.props.movie;
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath } = movie;
     return (
       <div className="movie-card movie-card-body">
-        <img className="movie-card-image" src={ imagePath } alt=""></img>
+        <img className="movie-card-image" src={ imagePath } alt="" />
         <h4 className="movie-card-title">{title}</h4>
         <h5 className="movie-card-subtitle">{subtitle}</h5>
         <p className="movie-card-storyline">{storyline}</p>
@@ -23,11 +24,11 @@ MovieCard.propTypes = {
     subtitle: PropTypes.string.isRequired,
     storyline: PropTypes.string.isRequired,
     imagePath: PropTypes.string.isRequired,
-  })
-}
+  }),
+};
 
 MovieCard.defaultProps = {
-  movie: {}
-}
+  movie: {},
+};
 
 export default MovieCard;
