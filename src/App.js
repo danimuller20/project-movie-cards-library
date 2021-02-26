a2b3e82d991d02d8dbf18a5234cf69c3a937d7f8
Marina Alane <marina.alane@outlook.com>
2021-02-26 11:03:31 -0300

    ajusta requisitos

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
 import './App.css';
+import movies from './data';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <MovieList movie={ movie } />
+        <MovieList movies={ movies } />
       </div>
     );
   }
