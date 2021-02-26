d4d12855adfe5ef2b764b0e3614146c9b3f193c7
LeonardoJosedaSilveira <rpgleo.js@gmail.com>
2021-02-26 05:06:50 -0300

    eitha commit

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
-    return <h1>rating</h1>;
+    const { rating } = this.props;
+    return <p>{ rating }</p>;
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
 
 export default Rating;
