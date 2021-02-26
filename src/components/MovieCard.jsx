9a4469892cffc431a3a26a8c9d1dc93a9d63e63e
Miguel Dantas <miiguelalb@gmail.com>
2021-02-26 12:20:29 -0300

    Corrigido erros no ESLint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,22 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Rating from './Rating';
 
-class MovieCard extends Component {
+class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
     const { title, subtitle, storyline, rating, imagePath } = movie;
     return (
       <div className="movie-card">
-        <img className="movie-card-image" src={ imagePath } alt={ title } />
-        <div className="movie-card-body">
-          <h4 className="movie-card-title">{title}</h4>
-          <h5 className="movie-card-subtitle">{subtitle}</h5>
-          <p className="movie-card-storyline">{storyline}</p>
+        <div>
+          <img src={ imagePath } alt={ title } className="movie-card-image" />
         </div>
+        <div className="card-titles">
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+        </div>
+        <p className="movie-card-storyline">{ storyline }</p>
         <Rating rating={ rating } />
       </div>
-    )
+    );
   }
 }
 
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import MovieCard from './MovieCard';
+import MovierCard from './MovieCard';
 
-class MovieList extends Component {
+class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
       <div className="movie-list">
-        {movies.map((movie) => <MovieCard movie={ movie } key={ movie.title } />)}
+        {movies.map((movie) => <MovierCard key={ movie.title } movie={ movie } />)}
       </div>
     );
   }
