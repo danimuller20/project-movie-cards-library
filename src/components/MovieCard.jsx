a9121a3c7f785ac0c99aa108967c8d1ade9eef23
ederson rodrigo <edersonrodrigo31@gmail.com>
2021-02-26 02:36:52 -0300

    rating component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class MovieCard extends React.Component {
       subtitle,
       storyline,
       imagePath,
+      rating,
     } = movie;
 
     return (
@@ -17,6 +19,7 @@ class MovieCard extends React.Component {
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
+        <Rating rating={ rating } />
       </div>
     );
   }
