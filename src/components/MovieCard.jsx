140f64c6df193fedfda54593a2638a8ce8fc22f9
Luiz Vicentin <luizpavanello@gmail.com>
2021-02-26 21:03:09 -0300

    11 - Create a component <Rating />

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -13,8 +13,9 @@ class MovieCard extends React.Component {
           <h4>{ title }</h4>
           <h5>{ subtitle }</h5>
           <p>{ storyline }</p>
-          <img src={ imagePath } alt= { title }/>
-        </div>  
+          <img src={ imagePath } alt={ title } />
+        </div>
+        <Rating rating={ rating } />
       </div>
     );
   }
