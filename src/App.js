1ceca20708e40b22491fdfecdba46ae46702857c
Luiz Vicentin <luizpavanello@gmail.com>
2021-02-26 21:48:59 -0300

    research sources

--- src/App.js
+++ src/App.js
@@ -14,3 +14,9 @@ function App() {
 }
 
 export default App;
+
+/* Source 1: https://scrimba.com/learn/learnreact */
+/* Source 2: https://tableless.com.br/guia-completo-react-ecossistema */
+/* Source 3:  https://bit.dev/components , https://www.newline.co/fullstack-react/30-days-of-react/ */
+/* Source 4: https://stackoverflow.com/questions/32325912/react-proptype-array-with-shape */
+/* Source 5: https://app.betrybe.com/course/front-end/react?use_case=side_bar */
