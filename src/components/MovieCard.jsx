7bdabac22aee9322457e7dcd34ea7705169cc1f6
Luiz Vicentin <luizpavanello@gmail.com>
2021-02-26 21:14:16 -0300

    Formating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,7 +1,7 @@
 // implement MovieCard component here
-import React from "react";
-import PropTypes from "prop-types";
-import Rating from "/Rating";
+import React from 'react';
+import PropTypes from 'prop-types';
+import Rating from '/Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -13,9 +13,9 @@ class MovieCard extends React.Component {
           <h4>{title}</h4>
           <h5>{subtitle}</h5>
           <p>{storyline}</p>
-          <img src={imagePath} alt={title} />
+          <img src={ imagePath } alt={ title } />
         </div>
-        <Rating rating={rating} />
+        <Rating rating={ rating } />
       </div>
     );
   }
