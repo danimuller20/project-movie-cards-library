cbcf279142285ddcf7f7035bad4defdc154b2510
Luiz Vicentin <luizpavanello@gmail.com>
2021-02-26 21:21:00 -0300

    Formating → 17 - Add PropTypes to all components

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,7 +1,7 @@
 // implement MovieCard component here
 import React from 'react';
 import PropTypes from 'prop-types';
-import Rating from '/Rating';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
