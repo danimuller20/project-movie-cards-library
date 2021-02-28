a5625924c04ac22cc60c5e52d885e72a25e3a81b
MichaelMascarenhas <michaelmascarenhastrybe@gmail.com>
2021-02-28 12:44:14 -0300

    atualizaçao code climate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,13 @@
 import React from 'react';
 
 class Header extends React.Component {
-    render() {
-        return ( 
-        <header>
-            <h1>Cabeçalho do projeto</h1>
-        </header>
-        )
-    }
+  render() {
+    return ( 
+      <header>
+        <h1>Cabeçalho do projeto</h1>
+      </header>
+    );
+  }
 }
 
 export default Header;
