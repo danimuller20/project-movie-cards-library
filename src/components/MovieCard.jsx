fdd827645744a1a85f2064b1124326ee7fbad8b0
MichaelMascarenhas <michaelmascarenhastrybe@gmail.com>
2021-02-28 12:54:38 -0300

    atualizaçao code climate

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 class MovieCard extends React.Component {
-    render () {
-        return (
-            <div>
+  render () {
+    return (
+      <div>
 
-            </div>
-        )
-    }
+      </div>
+    )
+  }
 }
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,14 +3,14 @@ import propTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
-    render () {
-        const  { movies } = this.props;
-        return(
-            <div>
-                { movies.map(movie => <MovieCard movie={ movie } key={ movie.title } />) }
-            </div>
-        )
-    }
+  render () {
+    const  { movies } = this.props;
+    return(
+      <div>
+        { movies.map(movie => <MovieCard movie={ movie } key={ movie.title } />) }
+      </div>
+    );
+  }
 }
 
 export default MovieList;
