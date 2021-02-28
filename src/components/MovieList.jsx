a4347e4f3ad75e0410c9ef2ad003d77b4a67a760
MichaelMascarenhas <michaelmascarenhastrybe@gmail.com>
2021-02-28 13:13:24 -0300

    code climate

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,9 +4,11 @@ import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    //const { movies } = this.props;
-    return(
-      <div></div>
+    // const { movies } = this.props;
+    return (
+      <div>
+          <MovieCard />
+      </div>
     );
   }
 }
