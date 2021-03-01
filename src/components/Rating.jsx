242274d4e3a1e1326b25ca24f785e10ee292d8d2
MarceloPelegrino <pelegrinocorrea@gmail>
2021-03-01 15:27:07 -0300

    Chegando ao final Dia 01-03-2021 15:27

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 class Rating extends React.Component {
   render() {
     const { rating } = this.props;
-    return <div className='rating'>{rating}</div>;
+    return <div className="rating">{rating}</div>;
   }
 }
 
