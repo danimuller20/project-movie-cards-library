c9b729f1c5818df78ca481923ca21e5070ab36f3
Jorge William Cardoso Furtado <jorge_william_cardoso@yahoo.com.br>
2021-03-01 12:26:08 -0300

    Envio base

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,22 @@
 // implement MovieList component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+// import { movies } from '../data';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <section className="movie-list">
+        {movies.map((movie) => <MovieCard movie={ movie } key={ movie.title } img={ movie.imagePath } rating={ movie.rating } />)}
+      </section>
+    );
+  }
+}
+
+// MovieCard.propTypes= {(
+// movi
+// )}
+
+export default MovieList;
