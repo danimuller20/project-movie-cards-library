984dfabf1b6ca291c657b95d3b0bd23733d052a3
MarceloPelegrino <pelegrinocorrea@gmail>
2021-03-01 12:38:08 -0300

    Chegando ao final Dia 01-03-2021 12:38

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,19 @@
 // implement MovieCard component here
+import React from 'react';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+    render() {
+        const { movie } = this.props;
+        const { imagePath, title, subtitle, storyline, rating } = movie;
+        return <div>
+            <img src={imagePath} alt='movie poster' />
+            <h4>{title}</h4>
+            <h5>{subtitle}</h5>
+            <p>{storyline}</p>
+            <Rating rating={rating} />
+        </div>
+    }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,29 @@
 // implement MovieList component here
 import React from 'react';
-// import movies from '../data';
+import MovieCard from './MovieCard';
+
+// 5 - Passe uma key para cada <MovieCard /> renderizado
+// <MovieList /> deve renderizar <MovieCard />s de forma dinâmica. Ou seja, deve utilizar a função map para renderizar uma lista. Cada componente <MovieCard /> deve receber uma prop key com o nome do filme.
+
+// O que será verificado:
+
+// Passe uma key para cada <MovieCard /> renderizado.
+
+// 4- Renderize componentes <MovieCard /> dentro de <MovieList />
+
+// <MovieList /> deve renderizar um componente <MovieCard /> para cada objeto contido no array
+//  recebido na prop movies.
+
+// O que será verificado:
+
+// Renderize componentes <MovieCard /> dentro de MovieList.
 
 class MovieList extends React.Component {
   render() {
-    return (
-      <div></div>
-    );
+    const { movies } = this.props
+    return (<div>
+      {movies.map(movie => <MovieCard key={movie.title} movie={movie} />)};
+    </div>)
   }
 }
 
