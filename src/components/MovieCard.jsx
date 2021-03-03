57c4a3eab8a28c1ad45fd8a4af6c9ba035c11ea8
Pedro Amaral <amaralph1995@gmail.com>
2021-03-03 19:46:01 -0300

    Cria component movieList

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,16 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,18 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    console.log(movies);
+
+    return (
+      <div>
+        {movies.map((movie) => <MovieCard movie={ movie } />)}
+      </div>
+    )
+  }
+}
+
+export default MovieList;
