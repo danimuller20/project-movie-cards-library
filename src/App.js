a8615f70501d623d5dea705af7febf63e1401154
Fabio Venancio Almeida <fvalmeida.@gmail.com>
2021-03-05 08:57:06 -0300

    segundo requisito, vamos ver o que o lint retorna

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-// texto         /* <MovieList movies={ Data } /> */
+// texto         /*  */
 import React from 'react';
 import Header from './components/Header';
-/* import MovieList from './components/MovieList';
-import Data from './data'; */
+import MovieList from './components/MovieList';
+import Data from './data';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <MovieList movies={ Data } />
       </div>
     );
   }
