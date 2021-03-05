3714eae651793e468f5ecab683610e6df26ede43
Fabio Venancio Almeida <fvalmeida.@gmail.com>
2021-03-05 07:38:02 -0300

    reiniciando projetp

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
+// texto         /* <MovieList movies={ Data } /> */
 import React from 'react';
-import './App.css';
 import Header from './components/Header';
-import MovieList from './components/MovieList';
-import Data from './data';
+/* import MovieList from './components/MovieList';
+import Data from './data'; */
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <MovieList movies={ Data } />
       </div>
     );
   }
 }
 
 export default App;
+
