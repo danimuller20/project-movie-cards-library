aec33d7adffedded2de57d163d6d8281bc5e6211
João Pedro Barros <joaopedrobarros5@gmail.com>
2021-03-07 18:13:59 -0300

    atualizando prop rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function Rating(rating) {
+export default function Rating(props) {
+  const { rating } = props;
   return (
     <article className="movie-card-rating">
       <div className="rating">{ rating }</div>
     </article>
   );
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
