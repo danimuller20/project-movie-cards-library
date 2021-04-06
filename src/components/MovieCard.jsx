b50265825c603582a07ad1669b9a8a0899413ac9
smylle <jjsilveira007@gmail.com>
2021-04-05 23:53:14 -0300

    Entendo o tal do React

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,16 @@
 // implement MovieCard component here
+import React from 'react';
+import movies from '../data';
+
+class MovieCard extends React.Component {
+    render() {
+        return (
+            <div>
+                { this.props.movies.map((movie) => <img src={movie.imagePath} alt={movie.title}/> )}
+
+            </div>
+        )
+    }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,18 @@
 // implement MovieList component here
+
+import React from 'react';
+import MovieCard from './MovieCard';
+import movies from '../data';
+
+class MovieList extends React.Component {
+    render() {
+        return (
+            <div>
+               <MovieCard movies={movies}/>
+               
+            </div>
+        )
+    }
+}
+
+export default MovieList;
