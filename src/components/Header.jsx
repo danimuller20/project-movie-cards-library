a501338c60c3e650affc3e8fba522c0c70ef563e
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2021-04-14 19:13:33 -0300

    Requisito 2 acho que foi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,10 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <h1>Movies Card Library</h1>
+      <header>
+        <h1>Movies Card Library</h1>
+      </header>
     );
   }
 }
-
 export default Header;
