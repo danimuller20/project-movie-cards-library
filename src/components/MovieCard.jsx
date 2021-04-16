6ef4e888e911a1e33bc548f409ee0fbdf0b9f0ca
FredericoCanoeira <fcanoeira@gmail.com>
2021-04-16 02:52:04 +0100

    Adiciona requisito 6

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,13 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCardRend extends React.Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      
+    );
+  }
+}
+
+export default MovieCardRend;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -7,18 +7,19 @@ class MovieListRend extends React.Component {
     const { movies } = this.props;
     return (
       <div>
-        {movies.map((Film) => <MovieCard />)}
+        {movies.map((Film) => <MovieCard key={ Film.title } movie={ Film } />)}
+
       </div>
     );
   }
 }
 
 MovieListRend.propTypes = {
-  movies: PropTypes.arrayOf( 
+  movies: PropTypes.arrayOf(
     PropTypes.shape({
       title: PropTypes.string,
-    }).isRequired
-  ).isRequired
+    }).isRequired,
+  ).isRequired,
 };
 
 export default MovieListRend;
