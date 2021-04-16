cc4416834cddfda3b6bdb271b00c80277d1ca226
FredericoCanoeira <fcanoeira@gmail.com>
2021-04-16 01:56:34 +0100

    Refatora requisito 1 e 2 e resolve 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieListRend extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div> 
+        
+      </div>
+    );
+  }
+}
+
+export default MovieListRend;
