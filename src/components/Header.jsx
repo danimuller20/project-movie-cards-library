83ddb4c12d97372e56407b5a0a31dc7f1940b9f8
GabrielLennox <meunomeelennox@gmail.com>
2021-04-23 01:20:35 -0300

    req 1 - cria componente header e o renderiza - skipa tests

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,13 @@
 // implement Header component here
 // Iniciando o projeto 🚀
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <h1>text</h1>
+    );
+  }
+}
+
+export default Header;
