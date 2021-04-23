e3053b5fa14f713b0d5b20eff57a8fc84239177a
GabrielLennox <meunomeelennox@gmail.com>
2021-04-23 03:03:26 -0300

    req 7 - renderiza imagem do filme e des-skipa testes do componente moviecard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
-    return ('');
+    return (
+      <div>
+        <img src={ imagePath } />
+      </div>
+    );
   }
 }
 
