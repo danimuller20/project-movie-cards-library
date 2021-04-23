312fad1fa041751e40f868efa827c18a1079d33a
GabrielLennox <meunomeelennox@gmail.com>
2021-04-23 02:55:25 -0300

    req 5 - passado uma key para cada moviecard e definido proptype do componente movielist

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return ('');
   }
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    return this.props.movies.map(movie => {
-      return <MovieCard />;
-    });
+    const { movies } = this.props;
+    return (
+      <div>
+        {movies.map((movie) => (
+          <MovieCard movie={ movie } key={ movie.title } />
+        ))}
+      </div>
+    );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
