2c6a2b497f538b998cc392d05d1d75db32b9a29f
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2021-04-23 08:38:57 -0300

    Implementando Rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { movies } = this.props;
-    const { imagePath, title, subtitle, storyline } = movies;
+    const { imagePath, title, subtitle, storyline, rating } = movies;
     return (
       <div>
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <img src={ imagePath } alt={ title } />
         <p>{ storyline }</p>
+        <Rating rating={ rating } />
       </div>
     );
   }
@@ -22,6 +24,7 @@ MovieCard.propTypes = {
     subtitle: PropTypes.string.isRequired,
     imagePath: PropTypes.string.isRequired,
     storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
   }).isRequired,
 };
 
